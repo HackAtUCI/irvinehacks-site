28b1db7890aa0136abe956cfd2af723694b1a0d2
Tyler Yu <66880934+tyleryy@users.noreply.github.com>
2023-11-11 13:20:26 -0800

    Removed unused Common component

--- apps/site/src/components/canvas/View.tsx
+++ apps/site/src/components/canvas/View.tsx
@@ -4,16 +4,6 @@ import { forwardRef, useImperativeHandle, useRef } from "react";
 import { OrbitControls, View as ViewImpl } from "@react-three/drei";
 import { Three } from "@/helpers/components/Three";
 
-// export const Common = ({ color }: any) => (
-// 	<Suspense fallback={null}>
-// 		{color && <color attach="background" args={[color]} />}
-// 		<ambientLight intensity={0.5} />
-// 		<pointLight position={[20, 30, 10]} intensity={1} />
-// 		<pointLight position={[-10, -10, -10]} color="blue" />
-// 		<PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
-// 	</Suspense>
-// );
-
 const View = forwardRef(({ children, orbit, ...props }: any, ref) => {
 	const localRef = useRef<any>();
 	useImperativeHandle(ref, () => localRef.current);

44753aa38e52d8d4dbf00dc9cb0646f365d7b12c
Sam Der <sder@uci.edu>
2023-11-11 16:08:33 -0800

    feat: add className prop to Button

--- apps/site/src/lib/components/Button/Button.tsx
+++ apps/site/src/lib/components/Button/Button.tsx
@@ -3,16 +3,17 @@ import styles from "./Button.module.css";
 interface ButtonProps {
 	text: string;
 	href?: string;
+	className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, href }) => {
+const Button: React.FC<ButtonProps> = ({ text, href, className }) => {
 	if (href) {
 		return (
 			<a
 				href={href}
 				target="_blank"
 				rel="noopener noreferrer"
-				className={styles.button + " font-body"}
+				className={styles.button + " font-body " + className}
 			>
 				{text}
 			</a>

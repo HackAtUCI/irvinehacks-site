6fea7e5c91a6c5236cba682c0e80283e1c752b9c
Alexander Liu <a@alexanderliu.com>
2023-11-22 14:04:00 -0800

    fix: replace HackUCI with IrvineHacks in copy

--- apps/site/src/app/(home)/sections/MentorVolunteer/MentorVolunteer.tsx
+++ apps/site/src/app/(home)/sections/MentorVolunteer/MentorVolunteer.tsx
@@ -8,14 +8,14 @@ const MentorVolunteer = () => {
 			<Stand
 				imageSrc={stand1}
 				header="Apply to be a Mentor"
-				description="Interested in inspiring the next generation of developers? Our mentors are vital to making HackUCI 2023 successful and enjoyable for our hackers."
+				description="Interested in inspiring the next generation of developers? Our mentors are vital to making IrvineHacks 2024 successful and enjoyable for our hackers."
 				buttonText="Mentor Application"
 				buttonHref="google.com"
 			/>
 			<Stand
 				imageSrc={stand2}
 				header="Become a Volunteer"
-				description="Want a peek behind the scenes? Join our team of event volunteers that make HackUCI run as smoothly as possible! Get access to all the free food and all our hackathon swag."
+				description="Want a peek behind the scenes? Join our team of event volunteers that make IrvineHacks run as smoothly as possible! Get access to all the free food and all our hackathon swag."
 				buttonText="Volunteer Form"
 				buttonHref="google.com"
 			/>

5ef1bf800202dae1f164b1383b5a7339fb085919
Sam Der <sder@uci.edu>
2023-11-25 18:10:57 -0800

    feat: separated landing background pictures

--- apps/site/src/app/(home)/sections/Landing/Landing.tsx
+++ apps/site/src/app/(home)/sections/Landing/Landing.tsx
@@ -2,13 +2,13 @@
 
 import { Suspense } from "react";
 import Image from "next/image";
+import clsx from "clsx";
 import { PerspectiveCamera } from "@react-three/drei";
 
 import { View } from "@/components/canvas/View";
 import Button from "@/lib/components/Button/Button";
 import Fireflies from "../../components/Fireflies";
 
-import landingBackground from "@/assets/backgrounds/landing-background.jpg";
 import fogLeft from "@/assets/images/fog-left.png";
 import fogRight from "@/assets/images/fog-right.png";
 
@@ -17,10 +17,10 @@ import styles from "./Landing.module.css";
 const Landing = () => {
 	return (
 		<section
-			style={{
-				backgroundImage: `url(${landingBackground.src})`,
-			}}
-			className="bg-cover bg-no-repeat bg-center"
+			className={clsx(
+				"bg-center bg-cover bg-no-repeat",
+				styles.landingBackground,
+			)}
 		>
 			<View className="absolute w-full h-full">
 				<Suspense fallback={null}>

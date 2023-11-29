ec9a3a27803b2525b272465cab803b5b33fa266a
Sam Der <sder@uci.edu>
2023-11-29 14:02:09 -0800

    feat: draft footer

---
+++ apps/site/src/lib/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import hack from "@/assets/logos/hack.svg";
+import mail from "@/assets/logos/hack.svg";
+import facebook from "@/assets/logos/hack.svg";
+import instagram from "@/assets/logos/hack.svg";
+import linkedin from "@/assets/logos/hack.svg";
+import youtube from "@/assets/logos/hack.svg";
+
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+	return <footer className={styles.footer}>Empty footer</footer>;
+};
+
+export default Footer;

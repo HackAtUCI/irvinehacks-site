e97f53b8e3169e62a8a35ece9360646659e89981
Sam Der <sder@uci.edu>
2023-12-11 15:23:33 -0800

    feat: Docker setup
    - Using a Docker Compose file, launch three containers. The first
    container is the MongoDB instance, the second container is a web
    application that allows easier interfacing with the instance, and the
    third is the backend.
    - The containers are not running on the host network, which is why
    the host of the Uvicorn server was changed to 0.0.0.0.

--- apps/api/src/dev.py
+++ apps/api/src/dev.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="localhost",
+        host="0.0.0.0",
         log_level="info",
         access_log=True,
         use_colors=True,

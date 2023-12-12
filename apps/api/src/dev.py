43ab98d3b84b9bab3c36c572ed6b313a4cc7eaab
Sam Der <sder@uci.edu>
2023-12-12 10:48:40 -0800

    update: use uvicorn to launch backend, not dev.py

--- apps/api/src/dev.py
+++ apps/api/src/dev.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="0.0.0.0",
+        host="localhost",
         log_level="info",
         access_log=True,
         use_colors=True,

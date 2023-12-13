eb6d869cfd0c652b030785b7d44c20931adb0f40
Tyler Yu <tyleryy@uci.edu>
2023-12-13 06:50:38 -0800

    fix: gradient touchups

--- apps/site/src/app/schedule/components/EventCard.tsx
+++ apps/site/src/app/schedule/components/EventCard.tsx
@@ -19,42 +19,44 @@ const EventCard = React.forwardRef<
 	React.ComponentPropsWithoutRef<typeof Card>
 >(({ className, ...props }, ref) => {
 	return (
-		<Card
-			ref={ref}
-			className={cn("w-2/3 flex flex-col m-5 border-2", className)}
-			{...props}
-		>
-			<CardHeader>
-				<CardTitle className="mb-2 text-3xl flex">
-					Event Name
-					<Badge
-						variant="outline"
-						className="ml-auto text-lg transition bg-gradient-to-tr from-red-500 via-blue-400 to-purple-500"
-					>
-						Badge
-					</Badge>
-				</CardTitle>
-				<Separator className="bg-white" />
-				<CardDescription className="text-lg">
-					Hosted By: XXX Organization
-				</CardDescription>
-				<CardDescription className="text-sm">
-					X:XX PM - X:XX PM PST | <a>Meeting Link</a>
-				</CardDescription>
-			</CardHeader>
-			<CardContent className="text-xl">
-				<p>
-					Short description of the event. Lorem ipsum dolor sit amet,
-					consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi
-					ut aliquip ex ea commodo consequat.{" "}
-				</p>
-			</CardContent>
-			<CardFooter className="ml-auto text-gray-300">
-				<p>Starting in XXX minutes</p>
-			</CardFooter>
-		</Card>
+		<>
+			<Card
+				ref={ref}
+				className={cn("w-2/3 flex flex-col m-5 border-2", className)}
+				{...props}
+			>
+				<CardHeader className="relative">
+					<CardTitle className="mb-2 text-3xl flex">
+						Event Name
+						<Badge
+							variant="outline"
+							className="ml-auto text-lg transition bg-gradient-to-tr from-red-500 via-blue-400 to-purple-500"
+						>
+							Badge
+						</Badge>
+					</CardTitle>
+					<Separator className="bg-white" />
+					<CardDescription className="text-lg">
+						Hosted By: XXX Organization
+					</CardDescription>
+					<CardDescription className="text-sm">
+						X:XX PM - X:XX PM PST | <a>Meeting Link</a>
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="text-xl">
+					<p>
+						Short description of the event. Lorem ipsum dolor sit
+						amet, consectetur adipiscing elit, sed do eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad
+						minim veniam, quis nostrud exercitation ullamco laboris
+						nisi ut aliquip ex ea commodo consequat.{" "}
+					</p>
+				</CardContent>
+				<CardFooter className="ml-auto text-gray-300">
+					<p>Starting in XXX minutes</p>
+				</CardFooter>
+			</Card>
+		</>
 	);
 });
 EventCard.displayName = "EventCard";
--- apps/site/src/app/schedule/page.tsx
+++ apps/site/src/app/schedule/page.tsx
@@ -11,14 +11,20 @@ export default function Schedule() {
 					<ShiftingCountdown />
 				</div>
 				<div className="flex flex-col justify-center items-center mb-10">
-					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-slate-500" />
+					<EventCard
+						className="hover:-translate-y-2 hover:scale-[102%] transition duration-150
+								ease-in-out bg-gradient-to-tl from-red-500 to-purple-500"
+					/>
 					<EventDiv
 						className="bg-green-600"
 						titleText="Hacking Starts"
 						subText="X:XX PM - X:XX PM PST"
 					/>
 					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-red-400" />
-					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-green-500" />
+					<EventCard
+						className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out
+					bg-gradient-to-r from-blue-300 to-blue-500 mix-blend-hard-light"
+					/>
 					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-orange-900" />
 					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out" />
 					<EventDiv

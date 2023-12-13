62ae24e7e8c320dfed483192735090dd4d4bc889
Tyler Yu <tyleryy@uci.edu>
2023-12-13 15:15:30 -0800

    feat: added props to EventCard component

--- apps/site/src/app/schedule/components/EventCard.tsx
+++ apps/site/src/app/schedule/components/EventCard.tsx
@@ -14,51 +14,88 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 
+interface EventCardProps {
+	titleText: string;
+	badgeText: string;
+	subText: string;
+	meetingLink: string;
+	time: string;
+	description: string;
+	footerText: string;
+	className?: string; // Optional className prop
+	badgeClassName?: string; // Optional className prop
+}
+
 const EventCard = React.forwardRef<
 	React.ElementRef<typeof Card>,
-	React.ComponentPropsWithoutRef<typeof Card>
->(({ className, ...props }, ref) => {
-	return (
-		<>
-			<Card
-				ref={ref}
-				className={cn("w-2/3 flex flex-col m-5 border-2", className)}
-				{...props}
-			>
-				<CardHeader className="relative">
-					<CardTitle className="mb-2 text-3xl flex">
-						Event Name
-						<Badge
-							variant="outline"
-							className="ml-auto text-lg transition bg-gradient-to-tr from-red-500 via-blue-400 to-purple-500"
-						>
-							Badge
-						</Badge>
-					</CardTitle>
-					<Separator className="bg-white" />
-					<CardDescription className="text-lg">
-						Hosted By: XXX Organization
-					</CardDescription>
-					<CardDescription className="text-sm">
-						X:XX PM - X:XX PM PST | <a>Meeting Link</a>
-					</CardDescription>
-				</CardHeader>
-				<CardContent className="text-xl">
-					<p>
-						Short description of the event. Lorem ipsum dolor sit
-						amet, consectetur adipiscing elit, sed do eiusmod tempor
-						incididunt ut labore et dolore magna aliqua. Ut enim ad
-						minim veniam, quis nostrud exercitation ullamco laboris
-						nisi ut aliquip ex ea commodo consequat.{" "}
-					</p>
-				</CardContent>
-				<CardFooter className="ml-auto text-gray-300">
-					<p>Starting in XXX minutes</p>
-				</CardFooter>
-			</Card>
-		</>
-	);
-});
+	EventCardProps
+>(
+	(
+		{
+			className,
+			badgeClassName,
+			titleText,
+			badgeText,
+			subText,
+			meetingLink,
+			time,
+			description,
+			footerText,
+			...props
+		},
+		ref,
+	) => {
+		return (
+			<>
+				<Card
+					ref={ref}
+					className={cn(
+						"w-2/3 flex flex-col m-5 border-2",
+						className,
+						"hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out",
+					)}
+					{...props}
+				>
+					<CardHeader className="relative">
+						<CardTitle className="mb-2 text-3xl flex items-baseline">
+							{titleText} <span className="m-3 text-4xl">|</span>
+							<p className="text-xl">{time}</p>
+							<Badge
+								variant="outline"
+								className={cn(
+									"ml-auto text-lg transition",
+									badgeClassName,
+								)}
+							>
+								{badgeText}
+							</Badge>
+						</CardTitle>
+						<Separator className="bg-white" />
+						<CardDescription className="text-lg">
+							{subText}
+						</CardDescription>
+						<CardDescription className="text-md">
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href={meetingLink}
+								className="text-blue-600"
+							>
+								Meeting Link
+							</a>
+						</CardDescription>
+					</CardHeader>
+					<CardContent className="text-xl">
+						<p>{description}</p>
+					</CardContent>
+					<CardFooter className="ml-auto text-gray-300">
+						<p>{footerText}</p>
+					</CardFooter>
+				</Card>
+			</>
+		);
+	},
+);
 EventCard.displayName = "EventCard";
 
 export default EventCard;
--- apps/site/src/app/schedule/page.tsx
+++ apps/site/src/app/schedule/page.tsx
@@ -4,6 +4,18 @@ import EventCard from "./components/EventCard";
 import EventDiv from "./components/EventDiv";
 export const revalidate = 60;
 
+const defaultCardProps = {
+	titleText: "Event Name",
+	badgeText: "Badge",
+	subText: "Hosted by: <Insert Organization Name>",
+	time: "X:XX PM - X:XX PM PST",
+	meetingLink: "https://www.google.com/",
+	description:
+		"Short description of the event. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+	footerText: "Starting in X minutes",
+	badgeClassName: "bg-slate-600",
+};
+
 export default function Schedule() {
 	return (
 		<>
@@ -13,21 +25,27 @@ export default function Schedule() {
 				</div>
 				<div className="flex flex-col justify-center items-center mb-10">
 					<EventCard
-						className="hover:-translate-y-2 hover:scale-[102%] transition duration-150
-								ease-in-out bg-gradient-to-tl from-red-500 to-purple-500"
+						{...defaultCardProps}
+						className="bg-zinc-700 bg-opacity-70 border-gray-400 border"
 					/>
 					<EventDiv
 						className="bg-green-600"
 						titleText="Hacking Starts"
 						subText="X:XX PM - X:XX PM PST"
 					/>
-					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-red-400" />
 					<EventCard
-						className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out
-					bg-gradient-to-r from-blue-300 to-blue-500 mix-blend-hard-light"
+						{...defaultCardProps}
+						className="bg-gray-500 bg-opacity-60 border-gray-400 border"
 					/>
-					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-orange-900" />
-					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out" />
+					<EventCard
+						{...defaultCardProps}
+						className="bg-gray-500 bg-opacity-60 border-gray-400 border"
+					/>
+					<EventCard
+						{...defaultCardProps}
+						className="bg-gray-500 bg-opacity-60 border-gray-400 border"
+					/>
+					<EventCard {...defaultCardProps} className="" />
 					<EventDiv
 						className="bg-red-700"
 						titleText="Hacking Ends"

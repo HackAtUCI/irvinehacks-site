fac230acd1c068b86541af669b9ca63c89ba67c0
Tyler Yu <tyleryy@uci.edu>
2023-12-13 05:43:48 -0800

    feat: shadcn deps + EventCard component

---
+++ apps/site/src/app/schedule/components/EventCard.tsx
@@ -0,0 +1,53 @@
+"use client";
+import * as React from "react";
+
+import { cn } from "@/lib/utils/utils";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+const EventCard = React.forwardRef<
+	React.ElementRef<typeof Card>,
+	React.ComponentPropsWithoutRef<typeof Card>
+>(({ className, ...props }, ref) => {
+	return (
+		<Card
+			ref={ref}
+			className={cn("w-2/3 flex flex-col m-5", className)}
+			{...props}
+		>
+			<CardHeader>
+				<CardTitle className="mb-2 text-3xl">Event Name</CardTitle>
+				<Separator className="bg-white" />
+				<CardDescription className="text-lg">
+					Hosted By: XXX Organization
+				</CardDescription>
+				<CardDescription className="text-sm">
+					X:XX PM - X:XX PM PST | Meeting Link
+				</CardDescription>
+			</CardHeader>
+			<CardContent className="text-xl">
+				<p>
+					Short description of the event. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi
+					ut aliquip ex ea commodo consequat.{" "}
+				</p>
+			</CardContent>
+			<CardFooter className="ml-auto text-gray-300">
+				<p>Starting in XXX minutes</p>
+			</CardFooter>
+		</Card>
+	);
+});
+EventCard.displayName = "EventCard";
+
+export default EventCard;
--- apps/site/src/app/schedule/page.tsx
+++ apps/site/src/app/schedule/page.tsx
@@ -1,5 +1,5 @@
 import ShiftingCountdown from "./components/ShiftingCountdown/ShiftingCountdown";
-
+import EventCard from "./components/EventCard";
 export const revalidate = 60;
 
 export default function Schedule() {
@@ -9,8 +9,12 @@ export default function Schedule() {
 				<div className="m-36">
 					<ShiftingCountdown />
 				</div>
-				<div className="h-96 flex justify-center align-middle">
-					Placeholder for Schedule
+				<div className="flex flex-col justify-center items-center mb-10">
+					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-slate-500" />
+					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-red-400" />
+					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-green-500" />
+					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out bg-orange-900" />
+					<EventCard className="hover:-translate-y-2 hover:scale-[102%] transition duration-150 ease-in-out" />
 				</div>
 			</section>
 		</>

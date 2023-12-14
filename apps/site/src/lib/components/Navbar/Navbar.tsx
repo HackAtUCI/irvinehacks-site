9fff156d18511e02f96593da65ffe5dea3620c69
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2023-12-14 15:53:32 -0800

    Update style changes to Navbar.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/lib/components/Navbar/Navbar.tsx
+++ apps/site/src/lib/components/Navbar/Navbar.tsx
@@ -68,7 +68,11 @@ function Navbar() {
 					<NavLinkItem href="/">Schedule</NavLinkItem>
 					<NavLinkItem href="/">Resources</NavLinkItem>
 					<NavLinkItem href="/">Stage</NavLinkItem> */}
-					<Button text="Login" href="/api/saml/login" isLightVersion />
+					<Button
+						text="Login"
+						href="/api/saml/login"
+						isLightVersion
+					/>
 				</NavMenu.List>
 			</div>
 		</NavMenu.Root>

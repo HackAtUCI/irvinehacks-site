703c09e68e3dd08ae0ffdda5d80f0e1d0c8c51a5
Taesung Hwang <taesungh@uci.edu>
2023-12-14 15:39:07 -0800

    Add typing overloads for SendGrid template data
    - Move `Template` enum from email_handler to sendgrid_handler
    - Include overloads to associate each SendGrid template with specific
      personalization data
    - Use overloads for `send_to_multiple` parameter which affects shape of
      provided `receiver_data`

--- apps/api/src/utils/email_handler.py
+++ apps/api/src/utils/email_handler.py
@@ -1,10 +1,9 @@
-from enum import Enum
 from typing import Protocol
 
 from pydantic import EmailStr
 
 from services import sendgrid_handler
-from services.sendgrid_handler import PersonalizationData
+from services.sendgrid_handler import Template
 
 IH_SENDER = ("apply@irvinehacks.com", "IrvineHacks 2024 Applications")
 
@@ -15,27 +14,27 @@ class ContactInfo(Protocol):
     last_name: str
 
 
-class Template(str, Enum):
-    # TODO: provide actual template IDs
-    CONFIRMATION_EMAIL = "d-2026cde7bebd45ad85723443808c5817"
-    GUEST_TOKEN = "d-b19f08e584cb4c0f97b55f567ee10afc"
-
-
 async def send_application_confirmation_email(user: ContactInfo) -> None:
     """Send a confirmation email after a user submits an application.
     Will propagate exceptions from SendGrid."""
-    send_data: PersonalizationData = {
-        "email": user.email,
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-    }
-    await sendgrid_handler.send_email(Template.CONFIRMATION_EMAIL, IH_SENDER, send_data)
+    await sendgrid_handler.send_email(
+        Template.CONFIRMATION_EMAIL,
+        IH_SENDER,
+        {
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        },
+    )
 
 
 async def send_guest_login_email(email: EmailStr, passphrase: str) -> None:
     """Email login passphrase to guest."""
-    send_data: PersonalizationData = {
-        "email": email,
-        "passphrase": passphrase,
-    }
-    await sendgrid_handler.send_email(Template.GUEST_TOKEN, IH_SENDER, send_data)
+    await sendgrid_handler.send_email(
+        Template.GUEST_TOKEN,
+        IH_SENDER,
+        {
+            "email": email,
+            "passphrase": passphrase,
+        },
+    )

3744c1da94f34edc648987725e6d651db6030b69
Taesung Hwang <taesungh@uci.edu>
2023-12-15 00:18:13 -0800

    Move `JWT_SECRET` check to `index.py`

--- apps/api/index.py
+++ apps/api/index.py
@@ -3,6 +3,10 @@ import logging
 from fastapi import FastAPI
 
 from app import app as api
+from auth.user_identity import JWT_SECRET
+
+if not JWT_SECRET:
+    raise RuntimeError("JWT_SECRET is not defined")
 
 # Override AWS Lambda logging configuration
 logging.basicConfig(level=logging.INFO, force=True)

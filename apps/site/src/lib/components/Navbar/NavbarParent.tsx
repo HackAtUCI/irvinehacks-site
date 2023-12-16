5c5ae295508fed7e0783a6486b1ea4e289b04257
Sam Der <sder@uci.edu>
2023-12-16 11:36:47 -0800

    fix: remove duplicate Identity declaration

--- apps/site/src/lib/components/Navbar/NavbarParent.tsx
+++ apps/site/src/lib/components/Navbar/NavbarParent.tsx
@@ -1,10 +1,5 @@
 import getUserIdentity from "@/lib/utils/getUserIdentity";
 import Navbar from "./Navbar";
-export interface Identity {
-    uid: string | null;
-    role: string | null;
-    status: string | null;
-}
 
 export default async function NavbarParent() {
     const identity = await getUserIdentity();

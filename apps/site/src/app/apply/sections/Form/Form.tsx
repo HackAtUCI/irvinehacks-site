6580e6f3737e97c619450f41cb9e7ceec1924e27
Sam Der <sder@uci.edu>
2023-12-15 16:42:12 -0800

    feat: enable application submission

--- apps/site/src/app/apply/sections/Form/Form.tsx
+++ apps/site/src/app/apply/sections/Form/Form.tsx
@@ -36,7 +36,9 @@ export default function Form() {
 				className={`${styles.image} absolute top-3/4 left-0`}
 			/>
 			<form
+				method="post"
 				className={`${styles.form} text-[#000000] w-8/12 flex flex-col items-center py-12 gap-10 z-1 max-[800px]:w-9/12 max-[400px]:w-11/12`}
+				action="/api/user/apply"
 			>
 				<BasicInformation />
 				<SchoolInformation />

59da0db442d5e8543add66135fcc4fdb42c9ac30
Ian Dai <ianbbdai@gmail.com>
2023-12-16 17:34:24 -0800

    Added back action endpoint for form

--- apps/site/src/app/apply/sections/Form/Form.tsx
+++ apps/site/src/app/apply/sections/Form/Form.tsx
@@ -39,6 +39,7 @@ export default function Form() {
 			<form
 				method="post"
 				className={`${styles.form} text-[#000000] w-8/12 flex flex-col items-center py-12 gap-10 z-1 max-[800px]:w-9/12 max-[400px]:w-11/12`}
+				action="/api/user/apply"
 				encType="multipart/form-data"
 			>
 				<BasicInformation />

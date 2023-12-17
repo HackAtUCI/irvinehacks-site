94879fa9f1902fa00c6a520149b5086dfe3a82f8
Sam Der <sder@uci.edu>
2023-12-17 14:16:50 -0800

    Set up login form (#114)
    * Set up login form:
    
    - Add styles to the page according to the Figma
    - Redirect to /portal if a user is already logged in
    - Added client-side form validation
    
    * fix: restore ValidatingForm, add validation SCSS
    
    * fix: SCSS select by class instead of element

--- apps/site/src/app/login/Login.tsx
+++ apps/site/src/app/login/Login.tsx
@@ -1,11 +1,26 @@
+import getUserIdentity from "@/lib/utils/getUserIdentity";
 import LoginForm from "./components/LoginForm";
 
-function Login() {
-	// TODO: check if user is already authenticated
+import water from "@/assets/backgrounds/water.jpg";
+import { redirect } from "next/navigation";
+
+async function Login() {
+	const identity = await getUserIdentity();
+	if (identity.uid !== null) redirect("/portal");
 
 	return (
-		<div className="min-h-screen flex flex-col justify-center bg-white text-black">
-			<h1>Log In</h1>
+		<div
+			className="min-h-screen flex flex-col items-center justify-center"
+			style={{ backgroundImage: `url("${water.src}")` }}
+		>
+			<h1
+				className="font-display text-5xl md:text-7xl mb-10"
+				style={{
+					textShadow: "0px 0px 20px rgba(255, 255, 255, 0.75);",
+				}}
+			>
+				Log In
+			</h1>
 			<LoginForm />
 		</div>
 	);

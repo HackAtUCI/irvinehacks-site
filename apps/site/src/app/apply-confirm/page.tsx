fa56b4b163e9afefaa689730a10278a0b22a62b1
Ian Dai <ianbbdai@gmail.com>
2023-12-17 00:53:26 -0800

    Created empty Title and Body

---
+++ apps/site/src/app/apply-confirm/page.tsx
@@ -0,0 +1,11 @@
+import Body from "./components/Body/Body";
+import Title from "./components/Title/Title";
+
+export default function ApplyConfirm() {
+	return (
+		<div>
+			<Title />
+			<Body />
+		</div>
+	);
+}

370deb98690c036530a045f7e3927736534bd179
Taesung Hwang <taesungh@uci.edu>
2023-12-17 15:43:56 -0800

    Refactor images in `Form` component on Apply page
    - Move images to parent `Apply` page component so the `Form` component
      is just the actual `<form>` element

--- apps/site/src/app/apply/page.tsx
+++ apps/site/src/app/apply/page.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
+import koiLeft from "@/assets/images/koi-swim-left.png";
+import koiRight from "@/assets/images/koi-swim-right.png";
+
 import Form from "./sections/Form/Form";
 import Title from "./sections/Title/Title";
+
+import styles from "./Apply.module.scss";
 
 export const revalidate = 60;
 
@@ -7,7 +14,33 @@ export default function Apply() {
 	return (
 		<div className="flex flex-col items-center gap-10 my-32">
 			<Title />
-			<Form />
+			<div className="relative w-full flex flex-col items-center">
+				<Image
+					src={koiLeft}
+					height="250"
+					alt="Koi fish"
+					className={`${styles.image} absolute top-0 right-0`}
+				/>
+				<Image
+					src={koiRight}
+					height="250"
+					alt="Koi fish"
+					className={`${styles.image} absolute top-1/4 left-0`}
+				/>
+				<Image
+					src={koiLeft}
+					height="250"
+					alt="Koi fish"
+					className={`${styles.image} absolute top-1/2 right-0`}
+				/>
+				<Image
+					src={koiRight}
+					height="250"
+					alt="Koi fish"
+					className={`${styles.image} absolute top-3/4 left-0`}
+				/>
+				<Form />
+			</div>
 		</div>
 	);
 }

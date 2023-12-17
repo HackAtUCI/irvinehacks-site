00c418ae2e2b22ba7c0857faff40eeb2741172b6
Ian Dai <ianbbdai@gmail.com>
2023-12-16 16:40:49 -0800

    Style changes to ResumeOutputFeedback

--- apps/site/src/app/apply/sections/Form/ResumeOutputFeedback.tsx
+++ apps/site/src/app/apply/sections/Form/ResumeOutputFeedback.tsx
@@ -1,20 +1,19 @@
-interface outputFeedbackProps {
+interface OutputFeedbackProps {
 	errorMessage: string;
 	resumePath: string;
 }
 
-export default function OutputFeedBack(props: outputFeedbackProps) {
-	if (props.errorMessage) {
-		return (
-			<span className="text-[#FF2222] text-xl">{props.errorMessage}</span>
-		);
+export default function OutputFeedBack({
+	errorMessage,
+	resumePath,
+}: OutputFeedbackProps) {
+	if (errorMessage) {
+		return <span className="text-[#FF2222] text-xl">{errorMessage}</span>;
 	}
 
 	return (
 		<span className="text-xl">
-			{props.resumePath
-				? "Successfully uploaded " + props.resumePath
-				: ""}
+			{resumePath ? "Successfully uploaded " + resumePath : ""}
 		</span>
 	);
 }

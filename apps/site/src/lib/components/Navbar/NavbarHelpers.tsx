a7eb3227f6b576b450ea81219edba8a528bd8a5d
Ian Dai <ianbbdai@gmail.com>
2023-12-18 01:17:21 -0800

    NavLinkItems are now composed of nextLinks

--- apps/site/src/lib/components/Navbar/NavbarHelpers.tsx
+++ apps/site/src/lib/components/Navbar/NavbarHelpers.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import React from "react";
+import Link from "next/link";
 import * as NavMenu from "@radix-ui/react-navigation-menu";
 
 import styles from "./Navbar.module.scss";
@@ -7,16 +8,18 @@ import styles from "./Navbar.module.scss";
 const NavLinkItem = React.forwardRef<
 	React.ElementRef<typeof NavMenu.Link>,
 	React.ComponentPropsWithoutRef<typeof NavMenu.Link>
->(({ children, className, ...props }, forwardedRef) => {
+>(({ children, className, href, ...props }, forwardedRef) => {
 	return (
 		<NavMenu.Item>
-			<NavMenu.Link
-				className={clsx(styles.navMenuLink, className)}
-				{...props}
-				ref={forwardedRef}
-			>
-				{children}
-			</NavMenu.Link>
+			<Link href={href as string} passHref legacyBehavior>
+				<NavMenu.Link
+					className={clsx(styles.navMenuLink, className)}
+					{...props}
+					ref={forwardedRef}
+				>
+					{children}
+				</NavMenu.Link>
+			</Link>
 		</NavMenu.Item>
 	);
 });

443becc3d6e8e6736c4616fb3a0e94ff0853ce03
Taesung Hwang <taesungh@uci.edu>
2023-12-17 17:16:08 -0800

    Implement client-side application form submission
    - Use axios to handle application submission on the client
      - Substitute write-in values marked as "other"
      - Provide means to handle unauthorized error (still todo)

--- apps/site/src/app/apply/sections/Form/Form.tsx
+++ apps/site/src/app/apply/sections/Form/Form.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { FormEvent } from "react";
+import axios from "axios";
+
 import Button from "@/lib/components/Button/Button";
 
 import BasicInformation from "./BasicInformation";
@@ -8,13 +13,49 @@ import ResumeInformation from "./ResumeInformation";
 
 import styles from "./Form.module.scss";
 
+const APPLY_PATH = "/api/user/apply";
+const FIELDS_WITH_OTHER = ["pronouns", "ethnicity", "school", "major"];
+
 export default function Form() {
+	const handleSubmit = async (
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> => {
+		// Disable native post submission
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+
+		// Use other values when selected
+		for (const field of FIELDS_WITH_OTHER) {
+			const otherField = `other_${field}`;
+			if (formData.get(field) === "other") {
+				formData.set(field, formData.get(otherField)!);
+				formData.delete(otherField);
+			}
+		}
+
+		const formEntries = Object.fromEntries(formData.entries());
+		console.debug(formEntries);
+
+		try {
+			const res = await axios.post(APPLY_PATH, formData);
+			if (res.status === 201) {
+				console.log("Application submitted");
+				// TODO: show submitted message
+			}
+		} catch (err) {
+			// TODO: unauthorized error
+			console.error(err);
+		}
+	};
+
 	return (
 		<form
 			method="post"
 			className={`${styles.form} text-[#000000] w-8/12 flex flex-col items-center py-12 gap-10 z-1 max-[800px]:w-9/12 max-[400px]:w-11/12`}
 			action="/api/user/apply"
 			encType="multipart/form-data"
+			onSubmit={handleSubmit}
 		>
 			<BasicInformation />
 			<SchoolInformation />

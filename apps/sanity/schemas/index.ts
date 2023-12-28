d1f84b4f5ebfec170d82775b55ab9dacbd31b320
Sam Der <sder@uci.edu>
2023-12-28 11:48:49 -0800

    Update Sanity resource schema:
    - Create new schema for resource categories
    - Adjusted category field in current resource schema to reference this
      new schema
    - Changed FAQ icon as it conflicts with the icon for the new schema

--- apps/sanity/schemas/index.ts
+++ apps/sanity/schemas/index.ts
@@ -1,6 +1,7 @@
 import faqs from "./faqs";
 import event from "./event";
 import resource from "./resource";
+import resourceCategory from "./resourceCategory";
 import sponsors from "./sponsors";
 
-export const schemaTypes = [faqs, event, resource, sponsors];
+export const schemaTypes = [faqs, event, resource, resourceCategory, sponsors];

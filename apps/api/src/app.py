65da2852542cd91a0ec187a228f5f316df8338e4
Taesung Hwang <taesungh@uci.edu>
2024-01-06 14:49:35 -0800

    Add local-only dev route to simulate user identity
    - Add dev router with `/impersonate` endpoint to simulate user identity
    - Mount dev router to app only when `DEPLOYMENT` environment is `LOCAL`
      - Add environment variable to docker-compose file
    - Include associated tests for dev router and mounting

--- apps/api/src/app.py
+++ apps/api/src/app.py
@@ -1,6 +1,11 @@
+import logging
+import os
+
 from fastapi import FastAPI
 
-from routers import demo, guest, saml, user, admin
+from routers import admin, demo, guest, saml, user
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
@@ -9,6 +14,11 @@ app.include_router(demo.router, prefix="/demo", tags=["demo"])
 app.include_router(guest.router, prefix="/guest", tags=["guest"])
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
+
+if os.getenv("DEPLOYMENT") == "LOCAL":
+    from routers import dev
+
+    app.include_router(dev.router, prefix="/dev")
 
 
 @app.get("/")
---
+++ apps/api/src/routers/dev.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+
+from auth import user_identity
+from auth.user_identity import COOKIE_NAME, NativeUser
+
+router = APIRouter()
+
+
+@router.get("/impersonate/{ucinetid}")
+async def impersonate(ucinetid: str) -> RedirectResponse:
+    """Simulate a user identity during local development (does not require https)."""
+    user = NativeUser(
+        ucinetid=ucinetid,
+        display_name="Local Dev",
+        email="dev@irvinehacks.com",
+        affiliations=[],
+    )
+
+    res = RedirectResponse("/portal", status_code=303)
+    jwt_token = user_identity._generate_jwt_token(user)
+    res.set_cookie(COOKIE_NAME, jwt_token, max_age=4000, httponly=True)
+    return res

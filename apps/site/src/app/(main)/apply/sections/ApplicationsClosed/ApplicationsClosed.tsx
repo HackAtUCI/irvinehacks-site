5171b93c03a266da8a9b15f016c72013d2fe33e5
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2024-01-14 18:59:22 -0800

    Application will not send if user is on application page and deadline passes (#224)
    * Form reloads if deadline has passed
    
    * ApplicationsClosed message will not display "already applied" if user hasnt applied
    
    * Added reload comment and removed unnecessary status check in applicationsclosed
    
    * Removed applicationsClosed status check

--- apps/site/src/app/(main)/apply/sections/ApplicationsClosed/ApplicationsClosed.tsx
+++ apps/site/src/app/(main)/apply/sections/ApplicationsClosed/ApplicationsClosed.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 
-function ApplicationsClosed() {
+import getUserIdentity from "@/lib/utils/getUserIdentity";
+
+async function ApplicationsClosed() {
+	const identity = await getUserIdentity();
+
 	return (
 		<div className="rounded-2xl p-10 text-display text-[#432810] bg-white text-center w-8/12 max-[800px]:w-9/12 max-[400px]:w-11/12">
 			<h1 className="text-4xl p-10 font-semibold max-[800px]:p-0">
 				Applications for IrvineHacks 2024 closed on January 14th.
 			</h1>
-			<hr className="mt-5 w-full h-0.5 bg-[#432810]" />
-			<p className="pt-10 text-lg">
-				If you already applied, please{" "}
-				<Link href="/login" className="text-blue-600 underline">
-					log in
-				</Link>{" "}
-				to visit your applicant portal.
-			</p>
+			<hr className="my-10 w-full h-0.5 bg-[#432810]" />
+			{identity.uid === null && (
+				<p className="text-lg">
+					If you already applied, please{" "}
+					<Link href="/login" className="text-blue-600 underline">
+						log in
+					</Link>{" "}
+					to visit your applicant portal.
+				</p>
+			)}
 			<p className="text-lg">
 				If you have any other questions or concerns, feel free to contact us at{" "}
 				<a href="mailto:hack@uci.edu" className="text-blue-600 underline">
--- apps/site/src/app/(main)/apply/sections/Form/Form.tsx
+++ apps/site/src/app/(main)/apply/sections/Form/Form.tsx
@@ -12,6 +12,7 @@ import ProfileInformation from "./ProfileInformation";
 import ResumeInformation from "./ResumeInformation";
 
 import styles from "./Form.module.scss";
+import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
 
 const APPLY_PATH = "/api/user/apply";
 const FIELDS_WITH_OTHER = ["pronouns", "ethnicity", "school", "major"];
@@ -25,6 +26,15 @@ export default function Form() {
 	): Promise<void> => {
 		// Disable native post submission
 		event.preventDefault();
+
+		// On the chance that a user presses submit after the deadline,
+		// this check is to prevent the application from submitting
+		// and to show the message that applications have closed
+		if (hasDeadlinePassed()) {
+			window.location.reload();
+			return;
+		}
+
 		setSubmitting(true);
 		setSessionExpired(false);
 
@@ -67,11 +77,7 @@ export default function Form() {
 	const sessionExpiredMessage = (
 		<p className="text-red-500 w-11/12">
 			Your session has expired. Please{" "}
-			<a
-				href="/login"
-				target="_blank"
-				className="text-blue-600 underline"
-			>
+			<a href="/login" target="_blank" className="text-blue-600 underline">
 				log in from a new tab
 			</a>{" "}
 			to restore your session and then try submitting again.

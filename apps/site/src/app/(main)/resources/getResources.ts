387edbdba76f54c0c31b6c97fa7a6430bd03acac
Nathan Huey <nate.huey4@gmail.com>
2024-01-15 20:39:15 -0800

    fix: fetch ResourceReference from sanity ref

--- apps/site/src/app/(main)/resources/getResources.ts
+++ apps/site/src/app/(main)/resources/getResources.ts
@@ -1,17 +1,26 @@
 import { z } from "zod";
 import { cache } from "react";
 import { client } from "@/lib/sanity/client";
-import { SanityDocument, SanityReference } from "@/lib/sanity/types";
+import { SanityDocument, SanityImageReference } from "@/lib/sanity/types";
+
+const ResourceReference = SanityDocument.extend({
+	_type: z.literal("resourceCategory"),
+	icon: SanityImageReference ,
+	description: z.string(),
+	title: z.string(),
+});
 
 const Resources = z.array(
 	SanityDocument.extend({
 		_type: z.literal("resource"),
 		link: z.string(),
 		title: z.string(),
-		resourceType: SanityReference,
+		resourceType: ResourceReference,
 	}),
 );
 
 export const getResources = cache(async () => {
-	return Resources.parse(await client.fetch("*[_type == 'resource']"));
+	return Resources.parse(
+		await client.fetch("*[_type == 'resource']{..., resourceType-> }"),
+	);
 });

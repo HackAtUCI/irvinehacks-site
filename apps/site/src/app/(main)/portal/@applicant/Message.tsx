d1cf7c7fcc2f52d22e871b2944f577f81559779d
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2024-01-15 20:12:47 -0800

    Created waitlisted and accepted messages for portal (#231)
    * Created waitlisted and accepted messages.
    
    * Updated apostrophe

--- apps/site/src/app/(main)/portal/@applicant/Message.tsx
+++ apps/site/src/app/(main)/portal/@applicant/Message.tsx
@@ -7,9 +7,8 @@ interface MessageProps {
 function Message({ status }: MessageProps) {
 	const submittedMessage = (
 		<p>
-			Thank you for submitting your application! We are currently
-			reviewing applications on a rolling basis, and you will hear back
-			from us soon!
+			Thank you for submitting your application! We are currently reviewing
+			applications on a rolling basis, and you will hear back from us soon!
 		</p>
 	);
 
@@ -18,15 +17,29 @@ function Message({ status }: MessageProps) {
 		[PortalStatus.reviewed]: submittedMessage,
 		[PortalStatus.rejected]: (
 			<p>
-				Thank you for applying to IrvineHacks this year. We have read
-				through many applications so far, and unfortunately are unable
-				to offer you a spot at our event. We highly encourage you to
-				continue developing your skills and passion for technology. We
-				would love to see you apply again next year!
+				Thank you for applying to IrvineHacks this year. We have read through
+				many applications so far, and unfortunately are unable to offer you a
+				spot at our event. We highly encourage you to continue developing your
+				skills and passion for technology. We would love to see you apply again
+				next year!
 			</p>
 		),
-		[PortalStatus.waitlisted]: <></>,
-		[PortalStatus.accepted]: <></>,
+		[PortalStatus.waitlisted]: (
+			<p>
+				Thank you for applying to IrvineHacks this year. We have read through
+				many applications so far, and are able to offer you a spot on the event
+				waitlist. Once a spot for IrvineHacks opens up, we will contact you via
+				email to confirm.
+			</p>
+		),
+		[PortalStatus.accepted]: (
+			<p>
+				Congratulations on your acceptance to IrvineHacks 2024! Please look for
+				an email confirming your acceptance. It is crucial that you read through
+				all additional info in your confirmation email. We look forward to
+				seeing you at IrvineHacks!
+			</p>
+		),
 		[PortalStatus.confirmed]: <></>,
 	};
 

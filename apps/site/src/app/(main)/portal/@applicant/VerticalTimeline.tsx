3a95f9c66c8245a02654e4d59d3a9fa8568dd13a
RandyHuynh5815 <randyhuynh5815@gmail.com>
2024-01-19 22:47:23 -0800

    feat: Displaying void message for applicants who failed to RSVP on time

--- apps/site/src/app/(main)/portal/@applicant/VerticalTimeline.tsx
+++ apps/site/src/app/(main)/portal/@applicant/VerticalTimeline.tsx
@@ -31,7 +31,9 @@ function VerticalTimeline({ status }: VerticalTimelineProps) {
 	);
 
 	const verdict_component =
-		status === PortalStatus.accepted || signedWaiver ? (
+		status === PortalStatus.accepted ||
+		status === PortalStatus.void ||
+		signedWaiver ? (
 			<li className="flex flex-row items-center border-t">
 				<Image
 					src={CheckCircle}
@@ -113,6 +115,17 @@ function VerticalTimeline({ status }: VerticalTimelineProps) {
 					className="m-6 mr-12"
 				/>
 				Attendance confirmed
+			</li>
+		) : status === PortalStatus.void ? (
+			<li className="flex flex-row items-center border-t">
+				<Image
+					src={XCircle}
+					alt="checked-circle"
+					width={25}
+					height={25}
+					className="m-6 mr-12"
+				/>
+				Sorry, you can not RSVP at this time.
 			</li>
 		) : null;
 

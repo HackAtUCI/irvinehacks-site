d74ea59e8795a9b6a68c37023ce51657cd733b1c
Taesung Hwang <taesungh@uci.edu>
2024-01-20 20:47:29 -0800

    Hide `ApplicantSummary` for non-managers
    - Show `ApplicantSummary` on Admin Dashboard only for application
      managers (director/reviewer)

--- apps/site/src/app/admin/dashboard/AdminDashboard.tsx
+++ apps/site/src/app/admin/dashboard/AdminDashboard.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import { useContext } from "react";
+
 import Container from "@cloudscape-design/components/container";
 import ContentLayout from "@cloudscape-design/components/content-layout";
 import SpaceBetween from "@cloudscape-design/components/space-between";
 
+import { isApplicationManager } from "@/lib/admin/authorization";
+import UserContext from "@/lib/admin/UserContext";
+
 import ApplicantSummary from "./components/ApplicantSummary";
 
 function AdminDashboard() {
+	const { role } = useContext(UserContext);
+
 	return (
 		<ContentLayout>
 			<SpaceBetween size="l">
 				<Container>Admin Dashboard</Container>
-				<ApplicantSummary />
+				{isApplicationManager(role) && <ApplicantSummary />}
 			</SpaceBetween>
 		</ContentLayout>
 	);

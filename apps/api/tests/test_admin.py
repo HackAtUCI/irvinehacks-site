a182b45cd58314a15a7759fa627f941c5f1987ef
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-01-20 22:39:34 -0800

    test description fix

--- apps/api/tests/test_admin.py
+++ apps/api/tests/test_admin.py
@@ -179,7 +179,7 @@ def test_confirm_attendance_when_status_waiver_signed(
     mock_mongodb_handler_retrieve_one: AsyncMock,
     mock_mognodb_handler_update: AsyncMock,
 ) -> None:
-    """Test that confirmed status changes to attending."""
+    """Test that confirmed status changes to void."""
 
     mock_mongodb_handler_retrieve_one.return_value = DIRECTOR_IDENTITY
     mock_mongodb_handler_retrieve.return_value = [
@@ -210,7 +210,7 @@ def test_confirm_attendance_when_status_accepted(
     mock_mongodb_handler_retrieve_one: AsyncMock,
     mock_mognodb_handler_update: AsyncMock,
 ) -> None:
-    """Test that confirmed status changes to attending."""
+    """Test that confirmed status changes to void."""
 
     mock_mongodb_handler_retrieve_one.return_value = DIRECTOR_IDENTITY
     mock_mongodb_handler_retrieve.return_value = [

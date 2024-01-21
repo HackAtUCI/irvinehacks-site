47923ba33216a1b16b9f03997cde469ca173a2f5
Taesung Hwang <taesungh@uci.edu>
2024-01-20 18:38:00 -0800

    Show **Dashboard** link in `AdminSidebar`
    - Remove divider for Applicants link and splice in when accessible

--- apps/site/src/app/admin/layout/AdminSidebar.tsx
+++ apps/site/src/app/admin/layout/AdminSidebar.tsx
@@ -6,9 +6,10 @@ import SideNavigation, {
 	SideNavigationProps,
 } from "@cloudscape-design/components/side-navigation";
 
-import { BASE_PATH, useFollowWithNextLink } from "./common";
 import { isApplicationManager } from "@/lib/admin/authorization";
 import UserContext from "@/lib/admin/UserContext";
+
+import { BASE_PATH, useFollowWithNextLink } from "./common";
 
 function AdminSidebar() {
 	const pathname = usePathname();
@@ -17,20 +18,18 @@ function AdminSidebar() {
 	const { role } = useContext(UserContext);
 
 	const navigationItems: SideNavigationProps.Item[] = [
+		{ type: "link", text: "Dashboard", href: "/admin/dashboard" },
 		{ type: "link", text: "Participants", href: "/admin/participants" },
 		{ type: "divider" },
 		{ type: "link", text: "Back to main site", href: "/" },
 	];
 
 	if (isApplicationManager(role)) {
-		navigationItems.unshift(
-			{
-				type: "link",
-				text: "Applicants",
-				href: "/admin/applicants",
-			},
-			{ type: "divider" },
-		);
+		navigationItems.splice(1, 0, {
+			type: "link",
+			text: "Applicants",
+			href: "/admin/applicants",
+		});
 	}
 
 	return (

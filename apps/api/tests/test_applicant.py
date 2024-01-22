78a28e238459c80c17880a1a916eded7021ffc9b
Sam Der <sder@uci.edu>
2024-01-21 20:00:10 -0800

    fix: type error

--- apps/api/tests/test_applicant.py
+++ apps/api/tests/test_applicant.py
@@ -5,9 +5,9 @@ from utils.user_record import Applicant, Status
 
 def test_key_uid_not_in_applicant_model_dump() -> None:
     test_applicant = Applicant(
-        _id="edu.uci.sder",
-        status=Status.ATTENDING,
+        uid="edu.uci.sder",
         application_data=EXPECTED_APPLICATION_DATA,
+        status=Status.ATTENDING,
     )
 
     assert "uid" not in test_applicant.model_dump()

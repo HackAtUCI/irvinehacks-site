a8d2f5a00673e1dedec922f302c1bc674c564675
Sam Der <sder@uci.edu>
2024-01-22 09:15:17 -0800

    fix: override model_dump instead of dict (#320)
    * fix: override model_dump instead of dict
    
    * feat: unit test
    
    * fix: type error

---
+++ apps/api/tests/test_applicant.py
@@ -0,0 +1,13 @@
+from test_user_apply import EXPECTED_APPLICATION_DATA
+
+from utils.user_record import Applicant, Status
+
+
+def test_key_uid_not_in_applicant_model_dump() -> None:
+    test_applicant = Applicant(
+        uid="edu.uci.sder",
+        application_data=EXPECTED_APPLICATION_DATA,
+        status=Status.ATTENDING,
+    )
+
+    assert "uid" not in test_applicant.model_dump()

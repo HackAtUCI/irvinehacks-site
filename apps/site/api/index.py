2b173addf312a407424b926ac335cf25d8fafdf2
Taesung Hwang <taesungh@uci.edu>
2024-11-24 22:50:20 -0800

    Disable deployment of API
    - Update build command for site
    - Remove `apps/site/api/index.py` and `functions` in `vercel.json`

--- apps/site/api/index.py
+++
@@ -1,5 +0,0 @@
-"""
-This is an empty source file necessary for Vercel to recognize the existence of
-the Serverless Function for the backend API as defined in `vercel.json`.
-The contents will be replaced by that of `apps/api/index.py` during Turbo build.
-"""

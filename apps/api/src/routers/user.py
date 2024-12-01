1a731b143fb8bbaa8fbb25e9c35ce626431d9ac4
IanWearsHat <ianbbdai@gmail.com>
2024-11-30 23:22:37 -0800

    Formatted document

--- apps/api/src/routers/user.py
+++ apps/api/src/routers/user.py
@@ -69,7 +69,7 @@ async def me(
     user_record = await mongodb_handler.retrieve_one(
         Collection.USERS, {"_id": user.uid}, ["role", "status"]
     )
-    
+
     if not user_record:
         return IdentityResponse(uid=user.uid)
 

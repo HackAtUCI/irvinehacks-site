b6c0587fed70bb9282642adca9c86ca16f309d4d
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-12-01 15:47:05 -0800

    fixed textbox positioning

--- apps/site/src/app/(main)/(home)/sections/Landing/LandingUnlocked.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/LandingUnlocked.tsx
@@ -16,15 +16,17 @@ import BgPillar from "@/assets/images/Desktop/pillars.png";
 import FarPillars from "@/assets/images/Desktop/clouds_distance_pillars.png";
 import BgCastle from "@/assets/images/Desktop/background_castle.png";
 import QuestBox from "@/assets/images/Desktop/text_box_with_title.svg";
+import LgStar from "@/assets/images/Desktop/large_star.svg";
+import SmStar from "@/assets/images/Desktop/small_star.svg";
 
 import styles from "./Landing.module.css";
 import Image from "next/image";
 
 const LandingUnlocked = () => {
 	return (
-		<section>
+		<section className={`${styles.landingUnlockedBackground} min-h-screen`}>
 			<div
-				className={`flex flex-col items-center overflow-hidden relative h-[1700px] max-lg:h-[1100px] max-sm:h-[600px]`}
+				className={`flex flex-col items-center overflow-hidden relative w-screen aspect-[3/4] max-h-[1750px] max-lg:h-[1100px] max-sm:h-[850px]`}
 			>
 				<View className="absolute w-full h-full">
 					<Suspense fallback={null}>
@@ -59,16 +61,16 @@ const LandingUnlocked = () => {
 					<Image
 						src={FarPillars}
 						alt="background pillars"
-						className={`absolute`}
+						className="absolute min-w-[570px]"
 					/>
 
 					<Image
 						src={BgCastle}
-						alt="background pillars"
-						className="absolute opacity-80"
+						alt="background castle"
+						className="absolute opacity-80 min-w-[520px]"
 					/>
 
-					<div className="w-full h-full absolute">
+					<div className="w-full h-full absolute min-w-[520px]">
 						<motion.div
 							initial={{ y: 200 }}
 							animate={{ y: 0, transition: { duration: 3 } }}
@@ -91,7 +93,7 @@ const LandingUnlocked = () => {
 					<Image
 						src={Cliff}
 						alt="anteater cliff"
-						className="absolute top-[31%] max-lg:top-[33%] min-w-[520px]"
+						className={`absolute top-[31%] max-lg:top-[33%] min-w-[520px]`}
 					/>
 					<Image
 						src={Cloud1}
@@ -104,33 +106,56 @@ const LandingUnlocked = () => {
 						className={`${styles.cloudAnim} absolute top-28 min-w-[1800px] max-lg:min-w-[150%] max-lg:top-0`}
 					/>
 				</div>
-				<div className="absolute bottom-0 w-80% aspect-[1260/470] flex flex-col items-center">
-					<p className="font-display text-3xl md:text-4xl font-bold mb-5">
-						3 Days, 400+ Hackers, $5,000+ in Prizes
-					</p>
+				<Image
+					src={Cloud2}
+					alt="clouds"
+					className={`absolute bottom-[-300px] min-w-[1800px] max-lg:min-w-[150%]`}
+				/>
+				<div className="w-full absolute bottom-0 flex justify-center aspect-[12/5]">
+					<Image src={LgStar} alt="*" className="absolute top-[30%] left-10" />
+					<Image
+						src={SmStar}
+						alt="*"
+						className="absolute bottom-[30%] left-[7%]"
+					/>
 
-					<div className="h-auto w-auto relative flex justify-center">
-						<p className="font-display text-3xl max-lg:text-xl mt-3 absolute">
-							Quest Unlocked
+					<Image src={LgStar} alt="*" className="absolute bottom-0 left-20" />
+
+					<Image src={LgStar} alt="*" className="absolute top-[40%] right-10" />
+					<Image
+						src={SmStar}
+						alt="*"
+						className="absolute bottom-[20%] right-[7%]"
+					/>
+					<div className="absolute bottom-0 flex flex-col items-center w-[75%] max-w-[1200px] min-w-[750px] pb-10">
+						<p className="font-display text-3xl md:text-4xl font-bold mb-10">
+							3 Days, 400+ Hackers, $5,000+ in Prizes
 						</p>
-						<div className="w-full h-full absolute text-center flex justify-center items-center">
-							<div className="w-[80%] h-[80%] flex flex-col justify-center items-center">
-								<p className="font-display text-2xl max-lg:text-base mt-3">
-									IrvineHacks is the largest collegiate hackathon in Orange
-									County and we continue expanding and improving our event every
-									year. Our focus? - Enhance the community around us by giving
-									students the platform to unleash their creativity in an
-									environment of forward thinking individuals.
-								</p>
-								<p className="font-display text-2xl max-lg:text-base mt-3">
-									This year, IrvineHacks will take place the weekend of January
-									26th to 28th. The event will be 100% in-person during the day
-									(not overnight). Free workshops, socials, food, and swag will
-									be provided
-								</p>
+
+						<div className="h-auto w-auto relative flex justify-center">
+							<div className="w-full h-full bg-white absolute mt-5 left-5"></div>
+							<Image src={QuestBox} alt="quest box" className="relative z-0" />
+							<p className="font-display text-3xl max-lg:text-xl mt-3 absolute">
+								Quest Unlocked
+							</p>
+							<div className="w-full h-full absolute text-center flex justify-center items-center z-1">
+								<div className="w-[80%] h-[80%] flex flex-col justify-center items-center relative">
+									<p className="font-display text-2xl max-xl:text-base max-2xl:text-xl mt-3">
+										IrvineHacks is the largest collegiate hackathon in Orange
+										County and we continue expanding and improving our event
+										every year. Our focus? - Enhance the community around us by
+										giving students the platform to unleash their creativity in
+										an environment of forward thinking individuals.
+									</p>
+									<p className="font-display text-2xl max-xl:text-base max-2xl:text-xl mt-3">
+										This year, IrvineHacks will take place the weekend of
+										January 26th to 28th. The event will be 100% in-person
+										during the day (not overnight). Free workshops, socials,
+										food, and swag will be provided
+									</p>
+								</div>
 							</div>
 						</div>
-						<Image src={QuestBox} alt="quest box" />
 					</div>
 				</div>
 			</div>

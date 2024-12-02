e5bc15266854b098360f64b77b8f43c91733ecc9
jotalis <jayqwu@gmail.com>
2024-12-02 01:32:38 -0800

    refactor: rebuild individual timeline components

---
+++ apps/site/src/app/(main)/portal/@applicant/components/Waiver.tsx
@@ -0,0 +1,32 @@
+import { TimelineComponent } from "./TimelineComponent";
+import { PortalStatus } from ".././ApplicantPortal";
+import { StatusImageProps } from "./StatusImage";
+
+export const WaiverComponent = ({ status }: { status: PortalStatus }) => {
+	let verdict = null;
+
+	if (status === PortalStatus.accepted) {
+		verdict = {
+			text: "Sign Waiver",
+			finished: false,
+			statusIcon: "Pending",
+		};
+	} else if (
+		status === PortalStatus.waived ||
+		status === PortalStatus.attending ||
+		status === PortalStatus.confirmed
+	) {
+		verdict = {
+			text: "Waiver Signed",
+			finished: true,
+			statusIcon: "Accepted",
+		};
+	}
+	return verdict ? (
+		<TimelineComponent
+			text={verdict.text}
+			finished={verdict.finished}
+			statusIcon={verdict.statusIcon as StatusImageProps["statusIcon"]}
+		/>
+	) : null;
+};

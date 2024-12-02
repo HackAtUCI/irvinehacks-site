13e274764c7aa34c6fc16d6e27df94038b8cc27c
jotalis <jayqwu@gmail.com>
2024-12-02 01:35:12 -0800

    refactor: cleanup vertical timeline component

--- apps/site/src/app/(main)/portal/@applicant/VerticalTimeline.tsx
+++ apps/site/src/app/(main)/portal/@applicant/VerticalTimeline.tsx
@@ -1,142 +1,21 @@
-import Image from "next/image";
-
-import BorderCircle from "@/assets/icons/border-circle.svg";
-import CheckCircle from "@/assets/icons/check-circle-fill.svg";
-import XCircle from "@/assets/icons/x-circle-fill.svg";
-import HalfCircle from "@/assets/icons/circle-half.svg";
-
+import React from "react";
 import { PortalStatus } from "./ApplicantPortal";
+import { SubmissionComponent } from "./components/Submission";
+import { VerdictComponent } from "./components/Verdict";
+import { WaiverComponent } from "./components/Waiver";
+import { RSVPComponent } from "./components/RSVP";
 
 interface VerticalTimelineProps {
 	status: PortalStatus;
 }
 
 function VerticalTimeline({ status }: VerticalTimelineProps) {
-	const attendanceConfirmed =
-		status === PortalStatus.confirmed || status === PortalStatus.attending;
-
-	const signedWaiver = status === PortalStatus.waived || attendanceConfirmed;
-
-	const submission_component = (
-		<li className="flex flex-row items-center">
-			<Image
-				src={CheckCircle}
-				alt="checked-circle"
-				width={25}
-				height={25}
-				className="m-6 mr-12"
-			/>
-			Application submitted
-		</li>
-	);
-
-	const verdict_component =
-		status === PortalStatus.accepted ||
-		status === PortalStatus.void ||
-		signedWaiver ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={CheckCircle}
-					alt="checked-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Application accepted
-			</li>
-		) : status === PortalStatus.rejected ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={XCircle}
-					alt="x-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Application rejected
-			</li>
-		) : status === PortalStatus.waitlisted ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={HalfCircle}
-					alt="half-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Application waitlisted
-			</li>
-		) : null;
-
-	const waiver_component =
-		status === PortalStatus.accepted ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={BorderCircle}
-					alt="border-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Sign waiver
-			</li>
-		) : signedWaiver ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={CheckCircle}
-					alt="checked-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Waiver signed
-			</li>
-		) : null;
-
-	const rsvp_component =
-		status === PortalStatus.accepted || status === PortalStatus.waived ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={BorderCircle}
-					alt="border-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Confirm attendance
-			</li>
-		) : attendanceConfirmed ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={CheckCircle}
-					alt="checked-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				Attendance confirmed
-			</li>
-		) : status === PortalStatus.void ? (
-			<li className="flex flex-row items-center border-t">
-				<Image
-					src={XCircle}
-					alt="x-circle"
-					width={25}
-					height={25}
-					className="m-6 mr-12"
-				/>
-				No RSVP Indicated
-			</li>
-		) : null;
-
 	return (
-		<div className="p-3">
-			<ul className="border rounded-lg">
-				{submission_component}
-				{verdict_component}
-				{waiver_component}
-				{rsvp_component}
-			</ul>
+		<div className="">
+			<SubmissionComponent />
+			<VerdictComponent status={status} />
+			<WaiverComponent status={status} />
+			<RSVPComponent status={status} />
 		</div>
 	);
 }

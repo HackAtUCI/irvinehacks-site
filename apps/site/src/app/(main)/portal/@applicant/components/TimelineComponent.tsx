b69c0d4e3a43cdb4514e7d39873bba9d27c45405
jotalis <jayqwu@gmail.com>
2024-12-02 03:04:33 -0800

    fix: remove top margins from timeline component

--- apps/site/src/app/(main)/portal/@applicant/components/TimelineComponent.tsx
+++ apps/site/src/app/(main)/portal/@applicant/components/TimelineComponent.tsx
@@ -12,14 +12,14 @@ export const TimelineComponent: React.FC<TimelineComponentProps> = ({
 	finished,
 	statusIcon,
 }) => {
-	const baseStyles = `flex items-center justify-between my-4 md:my-8 border-[2px] md:border-[5px] border-[var(--color-white)]`;
+	const baseStyles = `flex items-center justify-between mb-4 md:mb-8 border-[2px] md:border-[5px] border-[var(--color-white)]`;
 	const colorStyles = finished
 		? "text-[var(--color-white)] bg-[var(--color-black)]"
 		: "text-[var(--color-black)] bg-[var(--color-white)]";
 
 	return (
 		<div className={`${baseStyles} ${colorStyles}`}>
-			<h2 className="text-xs sm:text-base md:text-4xl leading-6 mx-4 sm:mx-8 md:mx-16 my-5 md:my-[70px]">
+			<h2 className="text-xs sm:text-base md:text-4xl mx-4 sm:mx-8 md:mx-16 my-5 md:my-[70px]">
 				{text}
 			</h2>
 			<StatusImage statusIcon={statusIcon} finished={finished} />

dff10850fc784379144a0ee632444ffbdedc5b01
Cyril Joby <cyriljoby@Cyrils-MacBook-Pro.local>
2024-12-02 22:03:49 -0800

    added hack logo and fixed bottom text

--- apps/site/src/lib/components/Footer/Footer.tsx
+++ apps/site/src/lib/components/Footer/Footer.tsx
@@ -4,63 +4,77 @@ import instagram from "@/assets/logos/instagram.svg";
 import Image from "next/image";
 import tiktok from "@/assets/logos/tiktok.svg";
 import redHeart from "@/assets/icons/red-heart.svg";
+import hack from "@/assets/logos/hack.svg"
 import styles from "./Footer.module.scss";
 
 const imageArray = [
 	{
-		"logo": mail,
-		"alt": "Email that directs to hack@uci.edu",
-		"link": "mailto:hack@uci.edu"
+		logo: hack,
+		alt: "Hack logo that links to Hack at UCI's website",
+		link: "https://hack.ics.uci.edu/",
 	},
 	{
-		"logo": discord,
-		"alt": "Discord logo that links to Hack at UCI's Discord invite",
-		"link": "https://discord.com/invite/pvkGxq2AWM"
+		logo: mail,
+		alt: "Email that directs to hack@uci.edu",
+		link: "mailto:hack@uci.edu",
 	},
 	{
-		"logo": instagram,
-		"alt": "Instagram logo that links to Hack at UCI's instagram",
-		"link": "https://www.instagram.com/hackatuci/"
+		logo: discord,
+		alt: "Discord logo that links to Hack at UCI's Discord invite",
+		link: "https://discord.com/invite/pvkGxq2AWM",
 	},
 	{
-		"logo": tiktok,
-		"alt": "Tiktok logo that links to Hack at UCI's tiktok",
-		"link": "https://www.tiktok.com/@hackatuci"
+		logo: instagram,
+		alt: "Instagram logo that links to Hack at UCI's instagram",
+		link: "https://www.instagram.com/hackatuci/",
 	},
-
-]
+	{
+		logo: tiktok,
+		alt: "Tiktok logo that links to Hack at UCI's tiktok",
+		link: "https://www.tiktok.com/@hackatuci",
+	}
+];
 
 const Footer = () => {
-
 	return (
-	<footer className={`${styles.footer} flex flex-col items-center`}>
-		<div className='flex flex-row footer-logos items-center gap-10 mt-12 max-[340px]:mt-5'>
-			{imageArray.map((item, index) => {
-				return(
-					<a key={`footer-logo-${index}`} href={item.link} target="_blank" rel="noopener noreferrer">
-						<Image
-							src={item.logo}
-							width="35"
-							alt={item.alt}
-						/>
-					</a>
-				)
-
-			})}
-		</div>
-		<div className="flex flex-row gap-1 items-center mt-2 max-[600px]:flex-col max-[210px]:gap-0 max-[210px]:mt-0">
-			<div className="flex flex-row gap-1 items-center max-[210px]:flex-col max-[210px]:gap-0">
-				<p className="m-0">Made with</p>
-				<Image src={redHeart} alt="Red heart icon" layout="intrinsic" />
-
-				<p className="m-0">in Irvine, CA <span className="max-[600px]:hidden">•</span></p>
+		<footer className={`${styles.footer} flex flex-col items-center`}>
+			<div className="flex flex-row footer-logos items-center gap-10 mt-12 max-[340px]:mt-5">
+				{imageArray.map((item, index) => {
+					return (
+						<a
+							key={`footer-logo-${index}`}
+							href={item.link}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src={item.logo} width="35" alt={item.alt} />
+						</a>
+					);
+				})}
 			</div>
+			<div className="flex flex-row gap-1 items-center mt-2 max-[600px]:flex-col max-[210px]:gap-0 max-[210px]:mt-0">
+				<div className="flex flex-row gap-1 items-center max-[210px]:flex-col max-[210px]:gap-0">
+					<p className="m-0">Made with</p>
+					<Image src={redHeart} alt="Red heart icon" layout="intrinsic" />
 
-			<p className="m-0 flex gap-1 whitespace-nowrap max-[340px]:flex-col max-[340px]:items-center">Learn more about the <a href="https://hack.ics.uci.edu" target="_blank" rel="noopener noreferrer"><span style={{"color" : "#FBA80A"}}>Hack @ UCI Team</span></a></p>
-		</div>
+					<p className="m-0">
+						by Hack at UCI <span className="max-[600px]:hidden">•</span>
+					</p>
+				</div>
 
-	</footer>
-	)
+				<p className="m-0 flex gap-1 whitespace-nowrap max-[340px]:flex-col max-[340px]:items-center">
+					Learn more about the{" "}
+					<a
+						href="https://hack.ics.uci.edu"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span style={{ color: "#FBA80A" }}>Hack @ UCI Team</span>
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
 	// ;#2878A6
 };
 

21fd0497758dea550c0aa7e55b5bb5dcbd2f83c4
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-12-03 19:42:12 -0800

    even more formatting fixes

--- apps/site/src/app/(main)/(home)/sections/About/About.tsx
+++ apps/site/src/app/(main)/(home)/sections/About/About.tsx
@@ -7,11 +7,7 @@ const About = () => {
 	return (
 		<>
 			<section className="container md mx-auto text-center my-14 max-w-sm sm:max-w-lg xl:max-w-screen-md relative">
-				<Lantern
-					width={300}
-					height={300}
-					className="lg:hidden mx-auto"
-				/>
+				<Lantern width={300} height={300} className="lg:hidden mx-auto" />
 				<Lantern
 					width={300}
 					height={300}
@@ -32,25 +28,21 @@ const About = () => {
 						<span className={styles.statistic}>400+</span> Hackers
 					</h2>
 					<h2>
-						<span className={styles.statistic}>$5,000+</span> in
-						Prizes
+						<span className={styles.statistic}>$5,000+</span> in Prizes
 					</h2>
 				</div>
-				<h3 className="text-xl lg:text-2xl mb-5">
-					Create + Connect + Inspire
-				</h3>
+				<h3 className="text-xl lg:text-2xl mb-5">Create + Connect + Inspire</h3>
 				<p>
-					IrvineHacks is the largest collegiate hackathon in Orange
-					County and we continue expanding and improving our event
-					every year. Our focus? – Enhance the community around us by
-					giving students the platform to unleash their creativity in
-					an environment of forward thinking individuals.
+					IrvineHacks is the largest collegiate hackathon in Orange County and
+					we continue expanding and improving our event every year. Our focus? –
+					Enhance the community around us by giving students the platform to
+					unleash their creativity in an environment of forward thinking
+					individuals.
 				</p>
 				<p>
-					This year, IrvineHacks will take place the weekend of
-					January 26th to 28th. The event will be 100% in-person
-					during the day (not overnight). Free workshops, socials,
-					food, and swag will be provided!
+					This year, IrvineHacks will take place the weekend of January 26th to
+					28th. The event will be 100% in-person during the day (not overnight).
+					Free workshops, socials, food, and swag will be provided!
 				</p>
 			</section>
 			<Image

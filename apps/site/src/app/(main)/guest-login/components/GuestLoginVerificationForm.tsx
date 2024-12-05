814571fda9457241381bb18d5451fe7e5c0e9719
IanWearsHat <ianbbdai@gmail.com>
2024-12-04 18:00:20 -0800

    Updated Guest Login page

--- apps/site/src/app/(main)/guest-login/components/GuestLoginVerificationForm.tsx
+++ apps/site/src/app/(main)/guest-login/components/GuestLoginVerificationForm.tsx
@@ -5,11 +5,12 @@ import clsx from "clsx";
 
 import ValidatingForm from "@/lib/components/ValidatingForm/ValidatingForm";
 import styles from "@/lib/components/ValidatingForm/ValidatingForm.module.scss";
+import RequiredAsterisk from "@/lib/components/forms/RequiredAsterisk";
 
 const VERIFICATION_PATH = "/api/guest/verify";
 const PASSPHRASE_REGEX = /\w+-\w+-\w+-\w+/;
 
-function VerificationForm() {
+export default function GuestLoginVerificationForm() {
 	const searchParams = useSearchParams();
 	const email = searchParams.get("email");
 
@@ -18,17 +19,13 @@ function VerificationForm() {
 	}
 
 	return (
-		<div className="bg-white p-5 md:p-10 rounded-2xl mx-5 text-black">
+		<div className="mx-8 md:m-0 p-6 md:px-10 md:py-8 border-[2px] md:border-[5px] border-[var(--color-white)] text-[var(--color-white)] bg-[var(--color-black)]">
 			<ValidatingForm method="post" action={VERIFICATION_PATH}>
-				<div className="flex flex-col mb-5">
-					<input
-						type="email"
-						name="email"
-						value={email}
-						readOnly
-						hidden
-					/>
-					<label htmlFor="passphrase">Passphrase</label>
+				<div className="flex flex-col mb-12 gap-2">
+					<input type="email" name="email" value={email} readOnly hidden />
+					<label htmlFor="passphrase">
+						Passphrase <RequiredAsterisk />
+					</label>
 					<input
 						id="passphrase"
 						className="bg-[#e1e1e1] p-1 rounded-2"
@@ -40,8 +37,8 @@ function VerificationForm() {
 						aria-describedby="passphrase-description"
 					/>
 					<small id="passphrase-description">
-						A login passphrase was sent to your email. Please enter
-						the passphrase.
+						A login passphrase was sent to your email. Please enter the
+						passphrase.
 					</small>
 					<p className={clsx(styles.invalidFeedback, "text-red-500")}>
 						Sorry, that passphrase is invalid.
@@ -51,5 +48,3 @@ function VerificationForm() {
 		</div>
 	);
 }
-
-export default VerificationForm;

653a2995f872da7a3d1452fe0889326e966f332b
jotalis <jayqwu@gmail.com>
2024-12-05 01:38:08 -0800

    feat: add portal bg stars

--- apps/site/src/app/(main)/portal/@applicant/ApplicantPortal.tsx
+++ apps/site/src/app/(main)/portal/@applicant/ApplicantPortal.tsx
@@ -7,6 +7,7 @@ import Message from "./components/Message";
 import SignWaiver from "./components/SignWaiver";
 import ReturnHome from "./components/ReturnHome";
 import VerticalTimeline from "./components/timeline/VerticalTimeline";
+import BackgroundStars from "./components/BackgroundStars";
 
 export const enum PortalStatus {
 	pending = "PENDING_REVIEW",
@@ -37,15 +38,21 @@ async function Portal() {
 	const rejected = status === PortalStatus.rejected;
 
 	return (
-		<div className="bg-transparent text-black max-w-6xl rounded-2xl p-6 flex flex-col mb-24 w-full">
-			<h2 className="font-bold font-display text-[var(--color-white)] mb-4 md:mb-[42px] text-[15px] sm:text-2xl md:text-[40px] md:leading-10">
-				Status
-			</h2>
-			<VerticalTimeline status={status as PortalStatus} />
-			<Message status={status as PortalStatus} />
-			{needsToSignWaiver && <SignWaiver />}
-			{submittedWaiver && <ConfirmAttendance status={status as PortalStatus} />}
-			{rejected && <ReturnHome />}
+		<div className="relative">
+			<BackgroundStars className="left-[-15%] top-[21%]" />
+			<div className="bg-transparent text-black max-w-6xl rounded-2xl p-6 flex flex-col mb-24 w-full">
+				<h2 className="font-bold font-display text-[var(--color-white)] mb-4 md:mb-[42px] text-[15px] sm:text-2xl md:text-[40px] md:leading-10">
+					Status
+				</h2>
+				<VerticalTimeline status={status as PortalStatus} />
+				<Message status={status as PortalStatus} />
+				{needsToSignWaiver && <SignWaiver />}
+				{submittedWaiver && (
+					<ConfirmAttendance status={status as PortalStatus} />
+				)}
+				{rejected && <ReturnHome />}
+			</div>
+			<BackgroundStars className="right-[-15%] bottom-[21%]" />
 		</div>
 	);
 }

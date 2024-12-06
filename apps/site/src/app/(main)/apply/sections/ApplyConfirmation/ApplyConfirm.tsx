e028a6eace10d2b859c3882de76ec000e8c6e6e2
IanWearsHat <ianbbdai@gmail.com>
2024-12-06 13:29:02 -0800

    Added continue href and role specific text to ApplyConfirm

--- apps/site/src/app/(main)/apply/sections/ApplyConfirmation/ApplyConfirm.tsx
+++ apps/site/src/app/(main)/apply/sections/ApplyConfirmation/ApplyConfirm.tsx
@@ -1,11 +1,27 @@
 import getUserIdentity from "@/lib/utils/getUserIdentity";
 import ConfirmationDetails from "./ConfirmationDetails";
 
-export default async function ApplyConfirm() {
+interface ApplyConfirmInterface {
+	continueHREF: string;
+	isNotHacker: boolean;
+}
+
+export default async function ApplyConfirm({
+	continueHREF,
+	isNotHacker,
+}: ApplyConfirmInterface) {
 	const identity = await getUserIdentity();
+	const roleText = isNotHacker
+		? "In addition, I understand that I must show up for my scheduled shifts."
+		: "In addition, I understand that I must check in at certain times on all three event days in order to be eligible to win prizes.";
+
 	return (
 		<div className="flex items-center w-screen h-screen">
-			<ConfirmationDetails isLoggedIn={identity.uid !== null} />
+			<ConfirmationDetails
+				continueHREF={continueHREF}
+				roleText={roleText}
+				isLoggedIn={identity.uid !== null}
+			/>
 		</div>
 	);
 }
--- apps/site/src/app/(main)/apply/sections/ApplyConfirmation/ConfirmationDetails.tsx
+++ apps/site/src/app/(main)/apply/sections/ApplyConfirmation/ConfirmationDetails.tsx
@@ -2,10 +2,14 @@ import Button from "@/lib/components/Button/Button";
 
 interface ConfirmationDetailsProps {
 	isLoggedIn: boolean;
+	continueHREF: string;
+	roleText: string;
 }
 
 export default async function ConfirmationDetails({
 	isLoggedIn,
+	continueHREF,
+	roleText,
 }: ConfirmationDetailsProps) {
 	return (
 		<div className="flex flex-col items-center gap-8 mx-20 p-6 md:px-10 md:py-8 border-[2px] md:border-[5px] border-[var(--color-white)] text-[var(--color-white)] bg-[var(--color-black)]">
@@ -14,18 +18,17 @@ export default async function ConfirmationDetails({
 				By submitting an application for IrvineHacks 2024, I understand that
 				IrvineHacks will take place in person during the day from January 26 to
 				28, and that IrvineHacks will not be providing transportation or
-				overnight accommodations. In addition, I understand that I must check in
-				at certain times on all three event days in order to be eligible to win
-				prizes. Lastly, I acknowledge that I am currently a student enrolled in
-				an accredited high school, college, or university in the United States
-				and will be over the age of 18 by January 26th, 2024.
+				overnight accommodations. {roleText} Lastly, I acknowledge that I am
+				currently a student enrolled in an accredited high school, college, or
+				university in the United States and will be over the age of 18 by
+				January 26th, 2024.
 			</p>
 			<strong className="text-lg text-[#FF2222]">
 				Applications are due on January 14th, 2024 at 11:59PM PST.
 			</strong>
 			<Button
 				text={isLoggedIn ? "Proceed to Application" : "Log in to Apply"}
-				href={isLoggedIn ? "/apply?prefaceAccepted=true" : "/login"}
+				href={isLoggedIn ? `${continueHREF}?prefaceAccepted=true` : "/login"}
 			/>
 			<hr className="mt-5 w-full h-0.5 bg-[#432810]" />
 			<p className="text-display">

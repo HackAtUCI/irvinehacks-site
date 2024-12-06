0108675d7467df546d3fdaa7ad7166b71ea86dfe
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2024-12-06 13:57:50 -0800

    Update Login UI (#418)
    * Updated main container
    
    * Styled button
    
    * Updated Guest Login page
    
    * Removed water background, added spam message
    
    * formatted Login.tsx
    
    * chore: rearrange imports
    
    ---------
    
    Co-authored-by: Albert Wang <alberw5@uci.edu>

--- apps/site/src/app/(main)/guest-login/components/GuestLoginVerificationForm.tsx
+++ apps/site/src/app/(main)/guest-login/components/GuestLoginVerificationForm.tsx
@@ -4,12 +4,14 @@ import { useSearchParams } from "next/navigation";
 import clsx from "clsx";
 
 import ValidatingForm from "@/lib/components/ValidatingForm/ValidatingForm";
+import RequiredAsterisk from "@/lib/components/forms/RequiredAsterisk";
+
 import styles from "@/lib/components/ValidatingForm/ValidatingForm.module.scss";
 
 const VERIFICATION_PATH = "/api/guest/verify";
 const PASSPHRASE_REGEX = /\w+-\w+-\w+-\w+/;
 
-function VerificationForm() {
+export default function GuestLoginVerificationForm() {
 	const searchParams = useSearchParams();
 	const email = searchParams.get("email");
 
@@ -18,11 +20,13 @@ function VerificationForm() {
 	}
 
 	return (
-		<div className="bg-white p-5 md:p-10 rounded-2xl mx-5 text-black">
+		<div className="mx-8 md:m-0 p-6 md:px-10 md:py-8 border-[2px] md:border-[5px] border-[var(--color-white)] text-[var(--color-white)] bg-[var(--color-black)]">
 			<ValidatingForm method="post" action={VERIFICATION_PATH}>
-				<div className="flex flex-col mb-5">
+				<div className="flex flex-col mb-12 gap-2">
 					<input type="email" name="email" value={email} readOnly hidden />
-					<label htmlFor="passphrase">Passphrase</label>
+					<label htmlFor="passphrase">
+						Passphrase <RequiredAsterisk />
+					</label>
 					<input
 						id="passphrase"
 						className="bg-[#e1e1e1] p-1 rounded-2"
@@ -37,6 +41,9 @@ function VerificationForm() {
 						A login passphrase was sent to your email. Please enter the
 						passphrase.
 					</small>
+					<span className="text-[#FF2222]">
+						If you cannot find the passphrase, please check your spam.
+					</span>
 					<p className={clsx(styles.invalidFeedback, "text-red-500")}>
 						Sorry, that passphrase is invalid.
 					</p>
@@ -45,5 +52,3 @@ function VerificationForm() {
 		</div>
 	);
 }
-
-export default VerificationForm;
--- apps/site/src/app/(main)/login/Login.tsx
+++ apps/site/src/app/(main)/login/Login.tsx
@@ -1,25 +1,15 @@
+import { redirect } from "next/navigation";
 import getUserIdentity from "@/lib/utils/getUserIdentity";
 import LoginForm from "./components/LoginForm";
-
-import water from "@/assets/backgrounds/water.jpg";
-import { redirect } from "next/navigation";
 
 async function Login() {
 	const identity = await getUserIdentity();
 	if (identity.uid !== null) redirect("/portal");
 
 	return (
-		<div
-			className="min-h-screen flex flex-col items-center justify-center"
-			style={{ backgroundImage: `url("${water.src}")` }}
-		>
-			<h1
-				className="font-display text-5xl md:text-7xl mb-10"
-				style={{
-					textShadow: "0px 0px 20px rgba(255, 255, 255, 0.75)",
-				}}
-			>
-				Log In
+		<div className="min-h-screen flex flex-col items-center justify-center">
+			<h1 className="font-display text-3xl md:text-5xl mb-20">
+				Login to Portal
 			</h1>
 			<LoginForm />
 		</div>

76207ed6ef14fdc65b7d2b7de79e7afaab7617c1
cyril <97067553+cyriljoby@users.noreply.github.com>
2024-12-06 05:18:30 -0800

    Updated Footer (#409)
    * footer with new svgs and updated socials
    
    * created red heart asset
    
    * added hack logo and fixed bottom text
    
    * formatted file
    
    * changed to official logos
    
    * instagram official logo
    
    * updated email
    
    ---------
    
    Co-authored-by: Cyril Joby <cyriljoby@Cyrils-MacBook-Pro.local>
    Co-authored-by: Albert Wang <alberw5@uci.edu>

--- apps/site/src/lib/components/Footer/Footer.tsx
+++ apps/site/src/lib/components/Footer/Footer.tsx
@@ -1,31 +1,37 @@
-import hack from "@/assets/logos/hack.svg";
 import mail from "@/assets/logos/mail.svg";
-import facebook from "@/assets/logos/facebook.svg";
+import discord from "@/assets/logos/discord.svg";
 import instagram from "@/assets/logos/instagram.svg";
 import Image from "next/image";
-
+import tiktok from "@/assets/logos/tiktok.svg";
+import redHeart from "@/assets/icons/red-heart.svg";
+import hack from "@/assets/logos/hack.svg";
 import styles from "./Footer.module.scss";
 
 const imageArray = [
 	{
 		logo: hack,
-		alt: "Hack Logo that links to Hack at UCI's website",
-		link: "https://hack.ics.uci.edu",
+		alt: "Hack logo that links to Hack at UCI's website",
+		link: "https://hack.ics.uci.edu/",
 	},
 	{
 		logo: mail,
-		alt: "Email that directs to hack@uci.edu",
-		link: "mailto:hack@uci.edu",
+		alt: "Email that directs to contact@irvinehacks.com",
+		link: "mailto:contact@irvinehacks.com",
 	},
 	{
-		logo: facebook,
-		alt: "Facebook logo that links to Hack at UCI's facebook",
-		link: "https://www.facebook.com/groups/HackAtUCI/",
+		logo: discord,
+		alt: "Discord logo that links to Hack at UCI's Discord invite",
+		link: "https://discord.com/invite/pvkGxq2AWM",
 	},
 	{
 		logo: instagram,
 		alt: "Instagram logo that links to Hack at UCI's instagram",
 		link: "https://www.instagram.com/hackatuci/",
+	},
+	{
+		logo: tiktok,
+		alt: "Tiktok logo that links to Hack at UCI's tiktok",
+		link: "https://www.tiktok.com/@hackatuci",
 	},
 ];
 
@@ -49,17 +55,10 @@ const Footer = () => {
 			<div className="flex flex-row gap-1 items-center mt-2 max-[600px]:flex-col max-[210px]:gap-0 max-[210px]:mt-0">
 				<div className="flex flex-row gap-1 items-center max-[210px]:flex-col max-[210px]:gap-0">
 					<p className="m-0">Made with</p>
-					<svg
-						className=" fill-white"
-						xmlns="http://www.w3.org/2000/svg"
-						height="16"
-						width="16"
-						viewBox="0 0 512 512"
-					>
-						<path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-					</svg>
+					<Image src={redHeart} alt="Red heart icon" layout="intrinsic" />
+
 					<p className="m-0">
-						in Irvine, CA <span className="max-[600px]:hidden">•</span>
+						by Hack at UCI <span className="max-[600px]:hidden">•</span>
 					</p>
 				</div>
 

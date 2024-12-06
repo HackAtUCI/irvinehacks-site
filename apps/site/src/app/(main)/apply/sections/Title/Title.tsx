1440a774811d279269cca09c762007efc5c33fd6
jotalis <jayqwu@gmail.com>
2024-12-06 12:26:12 -0800

    fix: pass in applicationType as a prop

--- apps/site/src/app/(main)/apply/sections/Title/Title.tsx
+++ apps/site/src/app/(main)/apply/sections/Title/Title.tsx
@@ -1,8 +1,12 @@
-export default function Title() {
+interface TitleProps {
+	applicationType: "Hacker" | "Mentor" | "Volunteer";
+}
+
+export default function Title({ applicationType }: TitleProps) {
 	return (
 		<>
 			<h1 className="text-[var(--color-white)] text-center font-display font-bold text-3xl md:text-5xl ">
-				Apply as a Hacker
+				Apply as a {applicationType}
 			</h1>
 			<h2 className="text-[var(--color-offwhite)] text-center font-body text-xl sm:text-2xl md:text-4xl lg:my-6">
 				Applications close on January 10th, 11:59PM PST

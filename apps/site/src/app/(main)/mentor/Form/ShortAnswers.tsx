8243d5525ffaa053b2697f408952f932a8fa67eb
IanWearsHat <ianbbdai@gmail.com>
2024-12-06 17:11:34 -0800

    Created base mentor form

---
+++ apps/site/src/app/(main)/mentor/Form/ShortAnswers.tsx
@@ -0,0 +1,37 @@
+import TextInput from "@/lib/components/forms/TextInput";
+import styles from "../../apply/sections/Form/Form.module.scss";
+import Textfield from "@/lib/components/forms/Textfield";
+
+export default function ShortAnswers() {
+	return (
+		<div className="flex flex-col gap-5 w-11/12">
+			<p className="text-4xl m-0 font-bold max-[700px]:text-3xl">
+				Profile Information
+			</p>
+			<Textfield
+				name="mentor_previous_experience"
+				labelClass={styles.label}
+				labelText="Have you participated or mentored at a hackathon before? If so, please list which ones. e.g. Hack at UCI 2024 (Hacker), ZotHacks 2024 (Mentor)"
+				inputClass="text-[var(--color-black)] bg-[#E1E1E1] p-3 h-48 resize-none rounded-xl"
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+			/>
+			<Textfield
+				name="mentor_interest"
+				labelClass={styles.label}
+				labelText="Why are you interested in being a mentor for IrvineHacks 2024? (100+ words recommended)"
+				inputClass="text-[var(--color-black)] bg-[#E1E1E1] p-3 h-48 resize-none rounded-xl"
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+			/>
+			<Textfield
+				name="mentor_team_help"
+				labelClass={styles.label}
+				labelText="How would you go about helping a team that is struggling with a bug? How would you go about helping a team that is struggling to work together?"
+				inputClass="text-[var(--color-black)] bg-[#E1E1E1] p-3 h-48 resize-none rounded-xl"
+				containerClass="flex flex-col w-full"
+				isRequired={true}
+			/>
+		</div>
+	);
+}
---
+++ apps/site/src/app/(main)/mentor/page.tsx
@@ -0,0 +1,61 @@
+import { redirect } from "next/navigation";
+
+import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
+import haveApplicationsOpened from "@/lib/utils/haveApplicationsOpened";
+
+import ApplyConfirm from "../apply/sections/ApplyConfirmation/ApplyConfirm";
+import Form from "./Form/Form";
+import Title from "../apply/sections/Title/Title";
+import getUserIdentity from "@/lib/utils/getUserIdentity";
+
+import ApplicationsClosed from "../apply/sections/ApplicationsClosed/ApplicationsClosed";
+
+export const revalidate = 60;
+
+export default async function Apply({
+	searchParams,
+}: {
+	searchParams?: {
+		prefaceAccepted?: string;
+	};
+}) {
+	const hasAcceptedQueryParam = searchParams?.prefaceAccepted === "true";
+	const identity = await getUserIdentity();
+
+	// if (identity.status !== null) {
+	// 	redirect("/portal");
+	// }
+
+	// if (hasAcceptedQueryParam && identity.uid === null) {
+	// 	redirect("/login");
+	// }
+
+	const deadlinePassed = hasDeadlinePassed();
+	const applicationsOpened = haveApplicationsOpened();
+	const applyBody = hasAcceptedQueryParam ? (
+		<>
+			<Title applicationType="Mentor" />
+			<div className="flex justify-center">
+				<Form />
+			</div>
+		</>
+	) : (
+		<ApplyConfirm continueHREF="/mentor" isHacker={false} />
+	);
+	return (
+		// <div className="flex flex-col items-center gap-10 my-32 min-h-[calc(100vh-8rem)]">
+		// 	{!applicationsOpened || deadlinePassed ? (
+		// 		<ApplicationsClosed />
+		// 	) : (
+		// 		applyBody
+		// 	)}
+		// </div>
+
+		<>
+			<Title applicationType="Mentor" />
+			<div className="flex justify-center">
+				<Form />
+			</div>
+		</>
+	);
+}

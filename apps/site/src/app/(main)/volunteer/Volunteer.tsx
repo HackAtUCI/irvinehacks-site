32e56f37a818d4ddc8bcc624292084b3e4494599
IanWearsHat <ianbbdai@gmail.com>
2024-12-07 04:01:36 -0800

    fixed various text styling and typos

--- apps/site/src/app/(main)/volunteer/Volunteer.tsx
+++ apps/site/src/app/(main)/volunteer/Volunteer.tsx
@@ -3,9 +3,11 @@ import VolunteerForm from "./VolunteerForm";
 const Volunteer = () => {
 	return (
 		<div className="w-full pb-10 flex flex-col items-center gap-12 mt-32">
-			<h1 className="font-display text-5xl font-bold">Apply as a Volunteer</h1>
-			<h2 className="font-display text-3xl text-yellow-100">
-				Applications close on January 30th, 11:59PM PST
+			<h1 className="font-display text-center text-5xl font-bold">
+				Apply as a Volunteer
+			</h1>
+			<h2 className="font-display text-center text-3xl text-yellow-100">
+				Applications close on January 10th, 11:59PM PST
 			</h2>
 			<div className="w-full flex justify-center pb-10">
 				<VolunteerForm />

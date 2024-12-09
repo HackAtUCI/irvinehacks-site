2eaccfdf9610ba573c40202e8b44824be61740d4
Ian Dai <ianbbdai@gmail.com>
2024-12-08 17:49:39 -0800

    Added generalized flow for applications

--- apps/api/src/routers/user.py
+++ apps/api/src/routers/user.py
@@ -9,7 +9,10 @@ from pydantic import BaseModel, EmailStr
 from auth import user_identity
 from auth.authorization import require_accepted_applicant
 from auth.user_identity import User, require_user_identity, use_user_identity
-from models.ApplicationData import ProcessedApplicationData, RawApplicationData
+from models.ApplicationData import (
+    ProcessedApplicationData,
+    RawApplicationData,
+)
 from models.user_record import Applicant, BareApplicant, Role, Status
 from services import docusign_handler, mongodb_handler
 from services.docusign_handler import WebhookPayload
@@ -67,14 +70,11 @@ async def me(
     return IdentityResponse(uid=user.uid, **user_record)
 
 
-@router.post("/apply", status_code=status.HTTP_201_CREATED)
-async def apply(
-    user: Annotated[User, Depends(require_user_identity)],
-    # media type should be automatically detected but seems like a bug as of now
-    raw_application_data: Annotated[
-        RawApplicationData, Form(media_type="multipart/form-data")
-    ],
-) -> str:
+async def apply_flow(
+    user: User,
+    raw_application_data: Union[RawApplicationData],
+    processed_application_type: Union[RawApplicationData],
+):
     if raw_application_data.application_type not in Role.__members__:
         raise HTTPException(
             status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -131,7 +131,7 @@ async def apply(
     else:
         resume_url = None
 
-    processed_application_data = ProcessedApplicationData(
+    processed_application_data = processed_application_type(
         **raw_app_data_dump,
         resume_url=resume_url,
         submission_time=now,
@@ -171,6 +171,20 @@ async def apply(
     return (
         "Thank you for submitting an application to IrvineHacks 2024! Please "
         + "visit https://irvinehacks.com/portal to see your application status."
+    )
+
+
+@router.post("/apply", status_code=status.HTTP_201_CREATED)
+async def apply(
+    user: Annotated[User, Depends(require_user_identity)],
+    # media type should be automatically detected but seems like a bug as of now
+    raw_application_data: Annotated[
+        RawApplicationData,
+        Form(media_type="multipart/form-data"),
+    ],
+) -> str:
+    return await apply_flow(
+        user, raw_application_data, processed_application_type=ProcessedApplicationData
     )
 
 

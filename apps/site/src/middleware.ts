a646f8db4dcc184c6ffea30f7f87a305c0d0f893
Taesung Hwang <taesungh@uci.edu>
2024-12-11 05:08:57 -0800

    Use Next middleware for Axiom only in production

--- apps/site/src/middleware.ts
+++ apps/site/src/middleware.ts
@@ -3,9 +3,11 @@ import { NextResponse } from "next/server";
 import type { NextFetchEvent, NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
-	const logger = new Logger({ source: "middleware" }); // traffic, request
-	await logger.middleware(request, { logRequestDetails: ["body"] });
+	if (process.env.VERCEL_ENV === "production") {
+		const logger = new Logger({ source: "middleware" }); // traffic, request
+		await logger.middleware(request, { logRequestDetails: ["body"] });
 
-	event.waitUntil(logger.flush());
+		event.waitUntil(logger.flush());
+	}
 	return NextResponse.next();
 }

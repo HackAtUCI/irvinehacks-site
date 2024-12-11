de787c8d6e4270299c1e871dc882788765e37df5
Ian Dai <ianbbdai@gmail.com>
2024-12-10 16:06:50 -0800

    Fixed isHacker prop

--- apps/site/src/app/(main)/volunteer/Volunteer.tsx
+++ apps/site/src/app/(main)/volunteer/Volunteer.tsx
@@ -15,7 +15,6 @@ export default async function Volunteer({
 			searchParams={searchParams}
 			applicationType="Volunteer"
 			applicationURL="/volunteer"
-			isHacker={false}
 		>
 			<VolunteerForm />
 		</ApplicationFlow>

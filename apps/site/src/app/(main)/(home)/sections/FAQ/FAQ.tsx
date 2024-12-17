e8bf60eaf46aeb27c2ab5a3199226d3e9c45dff4
Noah Kim <101167833+noahk004@users.noreply.github.com>
2024-12-16 22:13:53 -0800

    feature/faq (#428)
    * FAQ done
    
    * Revert pnpm-lock.yaml
    
    * Add rest of files
    
    * Clean up FAQ code
    
    * Minor styling changes
    
    * Fix misspellings in FAQ section
    
    * Refactor FAQ code
    
    * Change FAQ desktop view to appear at lg breakpoint instead of sm
    
    * Fix dialogue box sizes
    
    * Change spacing on FAQ section
    
    * FAQ apply prettier suggestions
    
    * Replace opacity classes with hidden for web accessibility
    
    * Remove duration class from FAQ
    
    * FAQ prettier changes

--- apps/site/src/app/(main)/(home)/sections/FAQ/FAQ.tsx
+++ apps/site/src/app/(main)/(home)/sections/FAQ/FAQ.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 import { getQuestions } from "./getQuestions";
-import FAQAccordion from "./FAQAccordion";
+import FAQAccordionMobile from "./FAQAccordionMobile";
+import FAQAccordionDesktop from "./FAQAccordionDesktop";
 
 import { PortableText } from "@portabletext/react";
-import styles from "./FAQ.module.scss";
+import sprite from "./assets/sprite.png";
+import organizerTitle from "./assets/organizer-title.png";
+
+export interface FAQAccordion {
+	faq: FAQ[];
+}
+
+export interface FAQ {
+	_key: string;
+	question: JSX.Element;
+	answer: JSX.Element;
+}
 
 const FAQ = async () => {
 	const questions = await getQuestions();
@@ -13,16 +27,39 @@ const FAQ = async () => {
 	}));
 
 	return (
-		<section
-			className={`${styles.container} rounded-[500rem/20rem] py-24 flex justify-center bg-no-repeat bg-cover bg-center bg-top`}
-		>
-			<div className="relative flex flex-col w-4/5 pb-7">
-				<h2
-					className={`${styles.title} my-6 font-display sm:text-[3rem] text-[#fffce2] text-4xl text-center`}
-				>
-					FAQ
+		<section className="flex justify-center bg-no-repeat bg-cover bg-top mt-20 lg:mt-40">
+			<div className="relative flex flex-col w-4/5 pb-7 justify-center">
+				<h2 className="my-6 font-display text-white text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl text-center">
+					Frequently Asked Questions
 				</h2>
-				<FAQAccordion faq={faq} />
+				<div className="mt-8 lg:mt-0 lg:flex lg:gap-6 lg:items-center lg:justify-center">
+					<div className="lg:flex lg:gap-3 lg:gap-[40px] lg:items-center">
+						<div className="flex justify-center items-center h-fit">
+							<Image
+								src={sprite}
+								alt="Wise anteater"
+								className={`
+									h-fit mt-[-30px] w-[250px] duration-300
+									sm:w-[300px] sm:min-w-[300px]
+									md:w-[400px] md:min-w-[400px]
+									lg:w-[240px] lg:min-w-[240px]
+									xl:w-[300px] xl:min-w-[300px]
+								`}
+							/>
+							<Image
+								src={organizerTitle}
+								alt="Wise anteater title"
+								className={`
+									hidden absolute lg:block h-fit duration-300
+									w-40 md:w-48 lg:w-60 xl:w-72
+									sm:top-[270px] md:top-[290px] lg:top-[330px] xl:top-[380px]
+								`}
+							/>
+						</div>
+						<FAQAccordionDesktop faq={faq} />
+						<FAQAccordionMobile faq={faq} />
+					</div>
+				</div>
 			</div>
 		</section>
 	);

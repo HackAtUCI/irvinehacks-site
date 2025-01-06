6b7246e4f5edaecc4a94f408f949f828de06424a
Albert Wang <alberw5@uci.edu>
2025-01-06 01:53:15 -0800

    Safari UI Fixes (#532)
    * fix focused question faq
    
    * fix faq unfocused question
    
    * fix desktop faq
    
    * fix apps
    
    * fix p cannot be a descendant of p error
    
    * fix page 2 width on xl screens
    
    * change partner blocks to be same height
    
    * remove unnecessary empty tag

--- apps/site/src/lib/components/forms/shared/Title/Title.tsx
+++ apps/site/src/lib/components/forms/shared/Title/Title.tsx
@@ -3,10 +3,12 @@ interface TitleProps {
 }
 
 export default function Title({ applicationType }: TitleProps) {
+	const applyTitle = `Apply as a ${applicationType}`;
+
 	return (
 		<>
 			<h1 className="text-[var(--color-white)] text-center font-display font-bold text-3xl md:text-5xl">
-				Apply as a {applicationType}
+				{applyTitle}
 			</h1>
 			<h2 className="text-[var(--color-offwhite)] text-center font-body text-xl my-6 sm:text-2xl md:text-4xl">
 				Applications close on January 10th, 11:59PM PST

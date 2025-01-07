7535748ca3a410b7a7abd633e8b5d57786c28f12
Albert Wang <alberw5@uci.edu>
2025-01-07 02:28:31 -0800

    add roles checkboxes and modal confirmation

---
+++ apps/site/src/app/admin/directors/AddOrganizerModal.tsx
@@ -0,0 +1,56 @@
+import Modal from "@cloudscape-design/components/modal";
+import Box from "@cloudscape-design/components/box";
+import Button from "@cloudscape-design/components/button";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import TextContent from "@cloudscape-design/components/text-content";
+import { RawOrganizer } from "./OrganizerInput";
+
+interface AddOrganizerModalProps {
+	onDismiss: () => void;
+	onConfirm: (organizer: RawOrganizer | null) => void;
+	organizer: RawOrganizer | null;
+}
+
+function AddOrganizerModal({
+	onDismiss,
+	onConfirm,
+	organizer,
+}: AddOrganizerModalProps) {
+	if (organizer === null) {
+		return <Modal visible={false} />;
+	}
+
+	return (
+		<Modal
+			onDismiss={onDismiss}
+			visible={true}
+			footer={
+				<Box float="right">
+					<SpaceBetween direction="horizontal" size="xs">
+						<Button variant="link" onClick={onDismiss}>
+							Cancel
+						</Button>
+						<Button variant="primary" onClick={() => onConfirm(organizer)}>
+							Add Organizer
+						</Button>
+					</SpaceBetween>
+				</Box>
+			}
+			header={`Add Organizer ${organizer.first_name} ${organizer.last_name} (${organizer.email})`}
+		>
+			<TextContent>
+				<p>
+					Double check that you have entered this organizer's info correctly
+				</p>
+				<ul>
+					<li>Email: {organizer.email}</li>
+					<li>First Name: {organizer.first_name}</li>
+					<li>Last Name: {organizer.last_name}</li>
+					<li>Newly Assigned Roles: {organizer.roles.join(", ")}</li>
+				</ul>
+			</TextContent>
+		</Modal>
+	);
+}
+
+export default AddOrganizerModal;
--- apps/site/src/app/admin/directors/OrganizerInput.tsx
+++ apps/site/src/app/admin/directors/OrganizerInput.tsx
@@ -7,6 +7,17 @@ import Input from "@cloudscape-design/components/input";
 import FormField from "@cloudscape-design/components/form-field";
 import Checkbox from "@cloudscape-design/components/checkbox";
 
+import AddOrganizerModal from "./AddOrganizerModal";
+
+const EMAIL_REGEX = /^\w+([\.\-]?\w+)*@\w+([\.\-]?\w+)*(\.\w{2,3})+$/;
+
+export interface RawOrganizer {
+	email: string;
+	first_name: string;
+	last_name: string;
+	roles: ReadonlyArray<string>;
+}
+
 function OrganizerInput() {
 	const [email, setEmail] = useState("");
 	const [firstName, setFirstName] = useState("");
@@ -14,8 +25,12 @@ function OrganizerInput() {
 	const [isHackerReviewer, setHackerReviewer] = useState(true);
 	const [isMentorReviewer, setMentorReviewer] = useState(false);
 	const [isVolunteerReviewer, setVolunteerReviewer] = useState(false);
+	const [organizer, setOrganizer] = useState<RawOrganizer | null>(null);
 
-	async function submitOrganizer() {
+	function updateOrganizer() {
+		// if (!EMAIL_REGEX.test(email)) {
+		// 	setE
+		// }
 		const roles = ["Organizer"];
 		if (isHackerReviewer) {
 			roles.push("Hacker Reviewer");
@@ -27,7 +42,7 @@ function OrganizerInput() {
 			roles.push("Volunteer Reviewer");
 		}
 
-		await axios.post("/api/director/organizers", {
+		setOrganizer({
 			email: email,
 			first_name: firstName,
 			last_name: lastName,
@@ -35,56 +50,67 @@ function OrganizerInput() {
 		});
 	}
 
+	async function submitOrganizer() {
+		await axios.post("/api/director/organizers", organizer);
+	}
+
 	return (
-		<SpaceBetween direction="horizontal" size="l">
-			<FormField label="Email">
-				<Input
-					onChange={({ detail }) => setEmail(detail.value)}
-					value={email}
-					inputMode="email"
-					placeholder="Organizer UCI Email"
-				/>
-			</FormField>
-			<FormField label="First Name">
-				<Input
-					onChange={({ detail }) => setFirstName(detail.value)}
-					value={firstName}
-					inputMode="text"
-					placeholder="First Name"
-				/>
-			</FormField>
-			<FormField label="Last Name">
-				<Input
-					onChange={({ detail }) => setLastName(detail.value)}
-					value={lastName}
-					inputMode="text"
-					placeholder="Last Name"
-				/>
-			</FormField>
-			<FormField label="Reviewer roles">
-				<Checkbox
-					onChange={({ detail }) => setHackerReviewer(detail.checked)}
-					checked={isHackerReviewer}
-				>
-					Hacker Reviewer
-				</Checkbox>
-				<Checkbox
-					onChange={({ detail }) => setMentorReviewer(detail.checked)}
-					checked={isMentorReviewer}
-				>
-					Mentor Reviewer
-				</Checkbox>
-				<Checkbox
-					onChange={({ detail }) => setVolunteerReviewer(detail.checked)}
-					checked={isVolunteerReviewer}
-				>
-					Volunteer Reviewer
-				</Checkbox>
-			</FormField>
-			<Button variant="primary" onClick={submitOrganizer}>
-				Add Organizer
-			</Button>
-		</SpaceBetween>
+		<>
+			<SpaceBetween direction="horizontal" size="l">
+				<FormField label="Email">
+					<Input
+						onChange={({ detail }) => setEmail(detail.value)}
+						value={email}
+						inputMode="email"
+						placeholder="Organizer UCI Email"
+					/>
+				</FormField>
+				<FormField label="First Name">
+					<Input
+						onChange={({ detail }) => setFirstName(detail.value)}
+						value={firstName}
+						inputMode="text"
+						placeholder="First Name"
+					/>
+				</FormField>
+				<FormField label="Last Name">
+					<Input
+						onChange={({ detail }) => setLastName(detail.value)}
+						value={lastName}
+						inputMode="text"
+						placeholder="Last Name"
+					/>
+				</FormField>
+				<FormField label="Reviewer roles">
+					<Checkbox
+						onChange={({ detail }) => setHackerReviewer(detail.checked)}
+						checked={isHackerReviewer}
+					>
+						Hacker Reviewer
+					</Checkbox>
+					<Checkbox
+						onChange={({ detail }) => setMentorReviewer(detail.checked)}
+						checked={isMentorReviewer}
+					>
+						Mentor Reviewer
+					</Checkbox>
+					<Checkbox
+						onChange={({ detail }) => setVolunteerReviewer(detail.checked)}
+						checked={isVolunteerReviewer}
+					>
+						Volunteer Reviewer
+					</Checkbox>
+				</FormField>
+				<Button variant="primary" onClick={updateOrganizer}>
+					Add Organizer
+				</Button>
+			</SpaceBetween>
+			<AddOrganizerModal
+				onDismiss={() => setOrganizer(null)}
+				onConfirm={submitOrganizer}
+				organizer={organizer}
+			/>
+		</>
 	);
 }
 

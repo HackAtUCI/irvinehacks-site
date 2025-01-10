059951e4bbadca33e41c8a433e9f7381a492511e
Ian Dai <ianbbdai@gmail.com>
2025-01-10 09:36:27 -0800

    Inverted isLoggedIn condition in Navbar

--- apps/site/src/lib/components/Navbar/Navbar.tsx
+++ apps/site/src/lib/components/Navbar/Navbar.tsx
@@ -6,7 +6,6 @@ import Button from "@/lib/components/Button/Button";
 import NavLinkItem from "./NavbarHelpers";
 
 // import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
-
 import buttonStyles from "@/lib/components/Button/Button.module.scss";
 import { Identity } from "@/lib/utils/getUserIdentity";
 import BaseNavbar from "./BaseNavbar";
@@ -17,20 +16,20 @@ interface NavbarProps {
 
 export default function Navbar({ identity }: NavbarProps) {
 	const { uid, status } = identity;
-	const isLoggedIn = uid === null;
+	const isLoggedIn = uid !== null;
 
 	return (
 		<BaseNavbar>
 			{status !== null && <NavLinkItem href="/portal">Portal</NavLinkItem>}
 			{isLoggedIn ? (
-				<Button text="Log In" href="/login" usePrefetch={false} isNavButton />
-			) : (
 				<a
 					href="/logout"
 					className={clsx(buttonStyles.buttonBox, buttonStyles.navButton)}
 				>
 					Log Out
 				</a>
+			) : (
+				<Button text="Log In" href="/login" usePrefetch={false} isNavButton />
 			)}
 		</BaseNavbar>
 	);

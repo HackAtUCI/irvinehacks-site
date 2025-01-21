5ed7006ad73ad3d4295ab7c460dabe64becacb97
Albert Wang <alberw5@uci.edu>
2025-01-20 19:16:05 -0800

    update frontend UI

--- apps/site/src/app/admin/directors/email-sender/components/Logistics.tsx
+++ apps/site/src/app/admin/directors/email-sender/components/Logistics.tsx
@@ -1,15 +1,21 @@
+import { SpaceBetween } from "@cloudscape-design/components";
 import SendGroup from "./SendGroup";
 
 function Logistics() {
 	return (
-		<SendGroup
-			description={"Send out logistics emails"}
-			buttonText="Send Logistics Emails"
-			modalText={
-				"You are about to send out logistics emails to hackers, mentors, and volunteers."
-			}
-			route="/api/director/logistics"
-		/>
+		<SpaceBetween size="m">
+			{["hackers", "mentors", "volunteers", "waitlists"].map((type, key) => {
+				return (
+					<SendGroup
+						key={key}
+						description={`Send out logistics emails to ${type}`}
+						buttonText="Send Logistics Emails"
+						modalText={`You are about to send out logistics emails to ${type}!`}
+						route={`/api/director/logistics/${type}`}
+					/>
+				);
+			})}
+		</SpaceBetween>
 	);
 }
 

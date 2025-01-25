02789206982b1978e01aeddaab089c9f2837a273
cyriljoby <cjoby@uci.edu>
2025-01-25 12:12:23 -0800

    moved brick up

--- apps/site/src/app/(main)/resources/page.tsx
+++ apps/site/src/app/(main)/resources/page.tsx
@@ -11,43 +11,43 @@ export const metadata: Metadata = {
 	title: "Resources | IrvineHacks 2025",
 };
 
-export default async function Resources() {
+export default function Resources() {
 	if (process.env.MAINTENANCE_MODE_RESOURCES) {
 		redirect("/");
 	}
 
 	return (
-		<>
-			<section className=" bg-black w-full h-full mb-12">
-				<div className="absolute top-[75%] left-0 w-full h-[calc(100%-75%)] min-h-[25vh] z-[-10]">
+		<section className="relative w-full min-h-screen overflow-hidden bg-black">
+			<div className="absolute inset-0 bg-black z-[-20]" />
+
+			<div className="relative w-full min-h-[400px] md:min-h-[500px] lg:min-h-[600px] flex flex-col items-center justify-center overflow-hidden z-10">
+				<Image
+					src={resource_header_bg}
+					alt="IrvineHacks 2025 Resources header"
+					fill
+					className="object-cover object-top"
+					priority
+				/>
+			</div>
+
+			<div className="text-center pt-8 pb-8 z-10">
+				<h1 className="mb-2 font-display font-bold text-4xl md:text-5xl">
+					Resources
+				</h1>
+			</div>
+			<div className="relative w-full z-0">
+				<div className="absolute bottom-0 left-0 w-full h-[50vh] z-[-10]">
 					<Image
 						src={brick_bg}
-						alt="Brick Background"
+						alt="Dark brick background"
 						fill
 						className="object-cover object-top"
-						priority
 					/>
 				</div>
-
-				<div className="relative w-full min-h-[400px] md:min-h-[500px] lg:min-h-[600px] flex flex-col items-center justify-center overflow-hidden">
-					<Image
-						src={resource_header_bg}
-						alt="Resources Header Background"
-						fill
-						className="object-cover object-top z-10"
-						priority
-					/>
-				</div>
-				<div className="text-center pt-8 pb-8">
-					<h1 className="mb-2 font-display font-bold text-4xl md:text-5xl">
-						Resources
-					</h1>
-				</div>
-
-				<div className="mx-4 mb-40">
+				<div className="relative mx-4 pb-16 z-10">
 					<ResourceSection />
 				</div>
-			</section>
-		</>
+			</div>
+		</section>
 	);
 }

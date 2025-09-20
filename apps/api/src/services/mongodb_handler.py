0e65eadd1de5043e2877cf7b2d9c4d2922f27c1a
ianbb <ianbbdai@gmail.com>
2025-09-20 12:32:17 -0700

    Exported constants for hackathon name

--- apps/api/src/services/mongodb_handler.py
+++ apps/api/src/services/mongodb_handler.py
@@ -9,7 +9,7 @@ from motor.core import AgnosticClient, AgnosticDatabase
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel, ConfigDict, Field
 
-from utils.hackathon_context import hackathon_name_ctx
+from utils.hackathon_context import hackathon_name_ctx, IRVINEHACKS, ZOTHACKS
 
 log = getLogger(__name__)
 
@@ -51,9 +51,9 @@ class Collection(str, Enum):
 
 def get_database() -> AgnosticDatabase[Any]:
     hackathon_name = hackathon_name_ctx.get()
-    if hackathon_name == "irvinehacks":
+    if hackathon_name == IRVINEHACKS:
         database_name = IRVINE_HACKS_DATABASE_NAME
-    elif hackathon_name == "zothacks":
+    elif hackathon_name == ZOTHACKS:
         database_name = ZOTHACKS_DATABASE_NAME
     else:
         raise ValueError(
--- apps/api/src/utils/hackathon_context.py
+++ apps/api/src/utils/hackathon_context.py
@@ -1,6 +1,10 @@
 from typing import Literal
 from contextvars import ContextVar
 
+
+IRVINEHACKS = "irvinehacks"
+ZOTHACKS = "zothacks"
+
 HackathonName = Literal["irvinehacks", "zothacks"]
 
 hackathon_name_ctx: ContextVar[HackathonName] = ContextVar(

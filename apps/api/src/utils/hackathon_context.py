5a25486613cefe7964436819ef724d555777aa50
ianbb <ianbbdai@gmail.com>
2025-09-20 12:33:05 -0700

    removed hackathon name variable

--- apps/api/src/utils/hackathon_context.py
+++ apps/api/src/utils/hackathon_context.py
@@ -5,8 +5,6 @@ from contextvars import ContextVar
 IRVINEHACKS = "irvinehacks"
 ZOTHACKS = "zothacks"
 
-HackathonName = Literal["irvinehacks", "zothacks"]
-
-hackathon_name_ctx: ContextVar[HackathonName] = ContextVar(
+hackathon_name_ctx: ContextVar[Literal["irvinehacks", "zothacks"]] = ContextVar(
     "hackathon_name", default="irvinehacks"
 )

22152dd13c98faa775dbe8aa9da944c2c791cfa7
ianbb <ianbbdai@gmail.com>
2026-01-02 13:49:20 -0800

    Created landing background

--- apps/site/src/app/(main)/(home)/sections/Landing/Landing.tsx
+++ apps/site/src/app/(main)/(home)/sections/Landing/Landing.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useRef } from "react";
+import Image from "next/image";
+import LandingBackground from "@/assets/backgrounds/landing-background.png";
+
 import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
 import haveApplicationsOpened from "@/lib/utils/haveApplicationsOpened";
 
@@ -17,30 +20,26 @@ const Landing = () => {
 
 	return (
 		<>
-			<section
-				className={`${
-					styles.landingBackground
-				} min-h-screen pb-[200px] relative overflow-hidden max-lg:h-[1500px]`}
-			>
+			<section>
 				<div
-					className={`flex flex-col items-center relative w-screen aspect-[3/4] max-h-[240vh] max-lg:h-[1100px] max-sm:h-[850px]`}
+					className={`min-h-screen absolute overflow-hidden z-10 w-full flex flex-col items-center justify-center`}
 				>
-					<div
-						className={`${styles.titleAnim} absolute z-10 w-full h-[50%] sm:h-[40%] flex flex-col items-center justify-center opacity-0`}
-					>
-						<div className="text-center relative p-10 flex flex-col items-center justify-center min-w-[200px]">
-							<div
-								className={`${styles.textDropshadow} w-full h-full absolute top-0 z-[-1]`}
-							/>
-							<h2 className="text-xl md:text-2xl mb-5 z-1">
-								February 27th - March 1st, 2026
-							</h2>
-							<h1 className="font-heading text-6xl md:text-7xl mb-5 max-sm:text-5xl">
-								IRVINEHACKS
-							</h1>
-						</div>
+					<div className="text-center relative p-10 flex flex-col items-center justify-center min-w-[200px]">
+						<h2 className="text-xl md:text-2xl mb-5 z-1">
+							February 27th - March 1st, 2026
+						</h2>
+						<h1
+							className={`${styles.headingDropShadow} font-heading text-6xl md:text-7xl mb-5 max-sm:text-5xl`}
+						>
+							IRVINEHACKS
+						</h1>
 					</div>
 				</div>
+				<Image
+					src={LandingBackground}
+					alt="landing background"
+					className="bg-[#00001c] w-screen"
+				/>
 			</section>
 			<div ref={scrollTo} />
 		</>

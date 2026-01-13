b5220eede0f55f17dfd3382b244d658cc7f19fbc
Nathan Ong <nathanong1217@gmail.com>
2026-01-13 12:44:34 -0800

    restyled event card

--- apps/site/src/app/(main)/schedule/components/EventCard.tsx
+++ apps/site/src/app/(main)/schedule/components/EventCard.tsx
@@ -23,7 +23,7 @@ export default function EventCard({
 	console.log(title, startTime.getTime() === endTime.getTime());
 	return (
 		<div
-			className={`w-[90%] min-w-[200px] h-full bg-black border-4 border-white relative p-16 font-display max-lg:w-full ${
+			className={`w-[90%] min-w-[200px] h-full bg-[#170f51]/50 border-[9px] border-[--color-yellow] relative p-12 font-display max-lg:w-full ${
 				isHappening &&
 				"max-lg:bg-blue-100 max-lg:border-blue-900 max-lg:text-blue-950 max-lg:top-[-8px] max-lg:left-[-8px]"
 			}`}
@@ -33,7 +33,7 @@ export default function EventCard({
 					<div>
 						<div className="flex justify-between gap-5 max-lg:flex-col">
 							<div className="h-fit w-full flex justify-between items-center">
-								<h1 className="text-4xl max-w-[80%] max-sm:text-3xl">
+								<h1 className="text-4xl [text-shadow:0_0_25px] max-w-[80%] max-sm:text-3xl">
 									{title}
 								</h1>
 								{isHappening && (
@@ -50,17 +50,16 @@ export default function EventCard({
 									</motion.div>
 								)}
 							</div>
-							<p className="text-2xl">{getTimeAndDates(startTime).day}</p>
 						</div>
-						<div className="pt-5">
+						<div className="pt-6 text-[--color-yellow]">
 							<div className="w-full flex justify-between gap-5 max-lg:flex-col ">
-								<p className="text-2xl">
+								<p className="text-2xl mb-0">
 									<span>Location: </span>
 									{virtual ? <a>Zoom</a> : location}
 								</p>
 								{organization && <p className="text-xl">By: {organization}</p>}
 							</div>
-							<p className="text-xl">{`Time: ${
+							<p className="text-2xl mt-0">{`Time: ${
 								startTime.getTime() === endTime.getTime()
 									? `${getTimeAndDates(endTime).compositeTimeHourMinute} ${
 											getTimeAndDates(endTime).amPm
@@ -71,18 +70,13 @@ export default function EventCard({
 							}`}</p>
 						</div>
 					</div>
-					<div
-						className={`w-full h-[2px] ${
-							isHappening ? "bg-blue-900" : "bg-white"
-						} mt-4 mb-4`}
-					/>
 					<div>
 						{hosts && (
 							<div className="text-2xl">{`Hosted By: ${hosts?.join(
 								", ",
 							)}`}</div>
 						)}
-						<div className="text-xl pt-5">{description}</div>
+						<div className="text-2xl text-[#FFFFFF] font-sans leading-[150%] font-normal pt-5">{description}</div>
 					</div>
 				</>
 			) : (

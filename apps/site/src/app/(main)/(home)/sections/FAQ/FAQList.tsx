b884762e4fc9660289ae42ef02b2f7b4727b1c3d
Andrew Hwang <148009680+pandyrew@users.noreply.github.com>
2026-01-21 16:53:54 -0800

    Add FAQ (#732)
    * Add FAQ
    
    * Use hack standard colors and restyle mobile view
    
    * remove unused FAQ components
    
    * prettier
    
    * fix mobile view

---
+++ apps/site/src/app/(main)/(home)/sections/FAQ/FAQList.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { PortableText } from "@portabletext/react";
+import { FAQ } from "./FAQ";
+
+const FAQList = ({ faqs }: { faqs: FAQ[] }) => {
+	const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+	return (
+		<div className="w-full pl-0 lg:pl-20 pt-3">
+			{faqs.map((faq, index) => (
+				<div key={faq._key}>
+					<button
+						type="button"
+						onClick={() =>
+							setSelectedIndex(selectedIndex === index ? null : index)
+						}
+						className="w-full text-left text-turquoise hover:underline flex items-start px-3 sm:px-4 py-3 transition-all duration-200"
+					>
+						<div className="w-14 sm:w-20 lg:w-24 xl:w-32 text-sm sm:text-base lg:text-lg flex-shrink-0">
+							{index + 1}.
+						</div>
+						<div className="flex-1 text-sm sm:text-base lg:text-lg">
+							{faq.question}
+						</div>
+					</button>
+
+					{selectedIndex === index && (
+						<div className="px-3 sm:px-4 py-4 sm:py-6 text-turquoise">
+							<div className="pl-0 sm:pl-20 lg:pl-24 xl:pl-32 text-xs sm:text-sm lg:text-base">
+								<PortableText value={faq.answer} />
+							</div>
+						</div>
+					)}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default FAQList;

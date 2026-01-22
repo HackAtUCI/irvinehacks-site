95c69cadda8826eef5a60591bd4145eb2119874e
Noah Kim <noahmk5002@gmail.com>
2026-01-21 17:33:20 -0800

    Change background images

--- apps/site/src/lib/components/forms/shared/ApplyConfirmation/ApplyConfirm.tsx
+++ apps/site/src/lib/components/forms/shared/ApplyConfirmation/ApplyConfirm.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 import getUserIdentity from "@/lib/utils/getUserIdentity";
 import ConfirmationDetails from "./ConfirmationDetails";
+
+import cityBackground from "@/assets/backgrounds/alt_illus_moonless.png";
 
 interface ApplyConfirmProps {
 	applicationURL: "/apply" | "/mentor" | "/volunteer";
@@ -28,11 +32,20 @@ export default async function ApplyConfirm({
 		role === "Mentor"
 			? mentorDetails
 			: role === "Volunteer"
-			  ? volunteerDetails // eslint-disable-line
-			  : ""; // eslint-disable-line
+				? volunteerDetails // eslint-disable-line
+				: ""; // eslint-disable-line
 
 	return (
 		<div className="flex items-center pt-24 pb-16">
+			<Image
+				src={cityBackground}
+				alt="Background image"
+				quality={100}
+				fill
+				sizes="100vh"
+				style={{ objectFit: 'cover', zIndex: -1 }} // cover ensures it covers the area and z-index places it behind content
+				priority
+			/>
 			<ConfirmationDetails
 				isLoggedIn={identity.uid !== null}
 				applicationURL={applicationURL}
--- apps/site/src/lib/components/forms/shared/BaseForm.tsx
+++ apps/site/src/lib/components/forms/shared/BaseForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { FormEvent, PropsWithChildren, useState } from "react";
+import Image from "next/image"
 import axios from "axios";
 
 import Button from "@/lib/components/Button/Button";
 
 import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
+
+import cityBackground from "@/assets/backgrounds/alt_illus_moonless.png"
 
 const FIELDS_WITH_OTHER = [
 	"pronouns",
@@ -101,11 +104,20 @@ export default function BaseForm({
 	return (
 		<form
 			method="post"
-			className="bg-black border-[4px] border-white text-[var(--color-white)] font-cantarell w-8/12 flex flex-col items-center py-12 gap-14 z-1 max-[800px]:w-9/12 max-[400px]:w-11/12"
+			className="bg-black border-[4px] border-white text-[var(--color-white)] w-8/12 flex flex-col items-center py-12 gap-14 z-1 max-[800px]:w-9/12 max-[400px]:w-11/12"
 			action={applyPath}
 			encType="multipart/form-data"
 			onSubmit={handleSubmit}
 		>
+			<Image
+				src={cityBackground}
+				alt="Background image"
+				quality={100}
+				fill
+				sizes="100vh"
+				style={{ objectFit: 'cover', zIndex: -1 }} // cover ensures it covers the area and z-index places it behind content
+				priority
+			/>
 			<input
 				type="text"
 				name="application_type"

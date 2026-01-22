fed731f56f70b69d3bbdfcc0dcca38d86190fdfb
ianbb <ianbbdai@gmail.com>
2026-01-22 01:51:08 -0800

    Hid navbar before release

--- apps/site/src/lib/components/Navbar/Navbar.tsx
+++ apps/site/src/lib/components/Navbar/Navbar.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-import clsx from "clsx";
+// import clsx from "clsx";
 
-import NavLinkItem from "./NavbarHelpers";
+// import NavLinkItem from "./NavbarHelpers";
 
 // import hasDeadlinePassed from "@/lib/utils/hasDeadlinePassed";
-import Button from "@/lib/components/Button/Button";
-import buttonStyles from "@/lib/components/Button/Button.module.scss";
-import { Identity } from "@/lib/utils/getUserIdentity";
+// import Button from "@/lib/components/Button/Button";
+// import buttonStyles from "@/lib/components/Button/Button.module.scss";
+// import { Identity } from "@/lib/utils/getUserIdentity";
 import BaseNavbar from "./BaseNavbar";
 
-interface NavbarProps {
-	identity: Identity;
-}
+// interface NavbarProps {
+// 	identity: Identity;
+// }
 
-export default function Navbar({ identity }: NavbarProps) {
-	const { uid, status } = identity;
-	const isLoggedIn = uid !== null;
+export default function Navbar() {
+	// const { uid, status } = identity;
+	// const isLoggedIn = uid !== null;
 
 	return (
 		<BaseNavbar>
-			{status !== null && <NavLinkItem href="/portal">Portal</NavLinkItem>}
+			{/* {status !== null && <NavLinkItem href="/portal">Portal</NavLinkItem>}
 			{isLoggedIn ? (
 				<a
 					href="/logout"
@@ -30,7 +30,7 @@ export default function Navbar({ identity }: NavbarProps) {
 				</a>
 			) : (
 				<Button text="Log In" href="/login" usePrefetch={false} isNavButton />
-			)}
+			)} */}
 		</BaseNavbar>
 	);
 }

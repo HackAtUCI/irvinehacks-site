98187050833562af661f9a00bd6334759ecb1a54
ianbb <ianbbdai@gmail.com>
2026-01-21 23:10:12 -0800

    Updated AgeInformation

--- apps/site/src/lib/components/forms/shared/AgeInformation.tsx
+++ apps/site/src/lib/components/forms/shared/AgeInformation.tsx
@@ -11,9 +11,9 @@ export default function AgeInformation() {
 			<div className="flex flex-col gap-5">
 				<p className="m-0 text-lg">
 					Because of limitations imposed by UCI, we are legally not allowed to
-					host minors (those under 18) for IrvineHacks 2025. By answering yes,
-					you affirm that you are and will be 18 years or older by January 24th,
-					2025.
+					host minors (those under 18) for IrvineHacks 2026. By answering yes,
+					you affirm that you are and will be 18 years or older by February
+					27th, 2026.
 				</p>
 				<p className="text-[#FF2222] m-0 text-lg">
 					We will be checking ID. If you are a minor, you will be turned away at
@@ -24,7 +24,7 @@ export default function AgeInformation() {
 			<SimpleRadio
 				name="is_18_older"
 				values={yesNoOptions}
-				title="Will you be 18 years or older by January 24th, 2025?"
+				title="Will you be 18 years or older by February 27th, 2026?"
 				titleClass="text-xl font-bold m-0 text-center"
 				containerClassTotal="flex flex-col gap-1 w-full items-center"
 				isRequired={true}

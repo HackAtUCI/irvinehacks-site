0aec6a28135dbe7889a1721778fb1fd1219e527e
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2026-01-21 17:06:52 -0800

    Updated login page to match the figma (#735)
    * Updated login page to match the figma
    
    * Update apps/site/src/app/(main)/login/components/LoginForm.module.scss
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Ran prettier on scss file
    
    * Resizing pink button on xs screens
    
    * Compressed login background to jpg
    
    * Fixed issues with pr
    
    * Update apps/site/src/lib/components/Button/Button.module.scss
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Removed margin from login-background
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ianbb <ianbbdai@gmail.com>

--- apps/site/src/app/(main)/login/components/LoginForm.tsx
+++ apps/site/src/app/(main)/login/components/LoginForm.tsx
@@ -1,8 +1,12 @@
 import clsx from "clsx";
+import Image from "next/image";
 
 import ValidatingForm from "@/lib/components/ValidatingForm/ValidatingForm";
 import RequiredAsterisk from "@/lib/components/forms/RequiredAsterisk";
 
+import Graphic from "@/assets/images/login-page-graphic.png";
+
+import loginFormStyles from "./LoginForm.module.scss";
 import styles from "@/lib/components/ValidatingForm/ValidatingForm.module.scss";
 
 // eslint-disable-next-line no-useless-escape
@@ -17,16 +21,21 @@ function LoginForm({ return_to }: { return_to?: string }) {
 	}
 
 	return (
-		<div className="mx-8 md:m-0 p-6 md:px-10 md:py-8 border-[2px] md:border-[5px] border-[var(--color-white)] text-[var(--color-white)] bg-[var(--color-black)]">
+		<div className="relative mx-8 md:m-0 p-6 md:px-10 md:py-8 border-[5px] border-[var(--color-yellow)] text-[var(--color-yellow)] bg-[#000] lg:w-[75%] w-[90%]">
+			<Image
+				src={Graphic}
+				alt="login page graphic"
+				className={loginFormStyles.loginFormGraphic}
+			/>
 			<ValidatingForm method="post" action={LOGIN_PATH + `?${searchParams}`}>
-				<div className="flex flex-col mb-12 gap-2">
+				<div className="flex flex-col mb-12 gap-2 w-full mt-8">
 					<label htmlFor="email">
 						Email <RequiredAsterisk />
 					</label>
 					<input
 						id="email"
 						type="email"
-						className="bg-[#e1e1e1] text-[var(--color-black)] p-1 rounded-2"
+						className="bg-[#FFF] text-[var(--color-black)] p-1 rounded-2 lg:h-12 md:h-10 "
 						pattern={EMAIL_REGEX.source}
 						name="email"
 						placeholder="Enter email"

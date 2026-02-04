613957b5dc8f48b63e1220bed955071f6d310576
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2026-02-03 21:02:42 -0800

    Finished up UI

--- apps/site/src/app/(main)/resources/components/ResourceItem.tsx
+++ apps/site/src/app/(main)/resources/components/ResourceItem.tsx
@@ -16,7 +16,7 @@ export default function ResourceItem({
 			key={key}
 			href={link}
 			target="_blank"
-			className="my-0.5 p-3 flex flex-col items-center justify-center text-xl sm:text-2xl text-white border border-2 border-yellow transition-all duration-300 hover:bg-white hover:text-black hover:border-white hover:scale-105"
+			className="bg-[rgba(20,40,90,0.45)] backdrop-blur-[12px] text-center w-full max-w-[300px] my-0.5 p-3 flex flex-col items-center justify-center text-xl sm:text-2xl text-white border border-2 border-yellow transition-all duration-300 hover:bg-white hover:text-black hover:scale-105"
 		>
 			{title}
 		</a>
@@ -25,7 +25,7 @@ export default function ResourceItem({
 			key={key}
 			href={link}
 			target="_blank"
-			className="my-2 flex flex-col items-center justify-center text-white border border-[3px] border-yellow transition-all duration-300 hover:bg-white hover:text-black hover:scale-105 w-full h-[200px] xl:h-[240px] flex-grow"
+			className="bg-[rgba(20,40,90,0.45)] backdrop-blur-[12px] my-2 flex flex-col items-center justify-center text-white border border-[3px] border-yellow transition-all duration-300 hover:bg-white hover:text-black hover:scale-105 w-[200px] lg:w-[225px] h-[200px] xl:h-[240px] flex-grow"
 		>
 			<div className="h-full py-6 xl:p-6">
 				{/* eslint-disable-next-line @next/next/no-img-element*/}
--- apps/site/src/app/(main)/resources/components/ResourceSection.tsx
+++ apps/site/src/app/(main)/resources/components/ResourceSection.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
-import ResourceCategory from "./ResourceCategory";
-import ResourceDescription from "./ResourceDescription";
+import Arrow from "@/assets/icons/resource-arrow.svg";
+import extraBorder from "@/assets/icons/ExtraBorder.svg";
+
 import ResourceItem from "./ResourceItem";
 import ResourceHeader from "./ResourceHeader"; 
 import { getResources } from "../getResources";
 import styles from "./ResourceSection.module.scss";
+import Image from "next/image";
 
 export default async function ResourceSection() {
 	const resources = await getResources();
@@ -14,42 +16,50 @@ export default async function ResourceSection() {
 			{resources.order.map(
 				({ _id, iconUrl, title, description, resources }) => (
 					<>
-						<div className="flex flex-col justify-center items-center">
-						<div className="bg-gradient-to-b from-[#00FFFF80] via-[#170F5180] to-[#00FFFF80] w-full max-w-5xl p-5 sm:p-7 grow-[2] mb-12 rounded-md select-none">
-								<ResourceHeader
-									title={title}
-									description={description}
-								/>
-								<div className="pb-[9px]">
-									<div className="block md:hidden">
-										<div className="mt-2 flex flex-col gap-4">
-											{resources.map(({ _id, resourceIconUrl, title, link }) => (
-												<ResourceItem
-													key={_id}
-													title={title}
-													link={link}
-													isMobile={true}
-												/>
-											))}
+						<div className="flex flex-col justify-center items-center mb-12">
+						<div className="flex flex-row items-center justify-center">
+							<Image src={extraBorder} alt="extra border" height={500} className={styles.leftBorder}/>
+							<div className="bg-gradient-to-b from-[#00FFFF80] via-[#170F5180] to-[#00FFFF80] w-full max-w-[400px] md:max-w-5xl p-5 sm:p-7 grow-[2] mb-12 rounded-md select-none">
+									<ResourceHeader
+										title={title}
+										description={description}
+									/>
+									<div className="pb-[9px]">
+										<div className="block md:hidden">
+											<div className="mt-2 flex flex-col gap-4 items-center justify-center">
+												{resources.map(({ _id, resourceIconUrl, title, link }) => (
+													<ResourceItem
+														key={_id}
+														title={title}
+														link={link}
+														isMobile={true}
+													/>
+												))}
+											</div>
 										</div>
-									</div>
-									<div className={clsx(
-											styles.scrollbar,
-											"hidden md:block overflow-y-auto h-[250px] xl:h-[275px] pb-[9px] pr-4",
-										)}>
-										<div className="p-5 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6 overflow-y-auto">
-											{resources.map(({ _id, resourceIconUrl, title, link }) => (
-												<ResourceItem
-													key={_id}
-													resourceIconUrl={resourceIconUrl}
-													title={title}
-													link={link}
-													isMobile={false}
-												/>
-											))}
+										<div className="hidden md:flex flex-row items-center justify-center">
+											<Image src={Arrow} alt="arrow" width={80} height={80} className="rotate-180" />
+											<div className={clsx(
+													styles.scrollbar,
+													"overflow-y-auto h-[250px] xl:h-[275px] pb-[9px] pr-4 mx-5",
+												)}>
+												<div className="p-5 w-full grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 overflow-y-auto">
+													{resources.map(({ _id, resourceIconUrl, title, link }) => (
+														<ResourceItem
+															key={_id}
+															resourceIconUrl={resourceIconUrl}
+															title={title}
+															link={link}
+															isMobile={false}
+														/>
+													))}
+												</div>
+											</div>
+											<Image src={Arrow} alt="arrow" width={80} height={80} />
 										</div>
 									</div>
 								</div>
+								<Image src={extraBorder} alt="extra border" height={500} className={styles.rightBorder} />
 							</div>
 						</div>
 					</>

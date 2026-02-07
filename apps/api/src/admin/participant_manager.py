49af6a40894861ced76c031df05d4c0fb9271555
connieyyy <con.yang4098@gmail.com>
2026-02-07 13:07:43 -0800

    updating decision logic for adding participant to queue

--- apps/api/src/admin/participant_manager.py
+++ apps/api/src/admin/participant_manager.py
@@ -124,18 +124,21 @@ async def add_participant_to_queue(uid: str, associate: User) -> None:
 
     if record is None:
         raise ValueError("No application record found.")
+    
+    status = record.get("status")
+    decision = record.get("decision")
     roles = record.get("roles")
 
     if Role.HACKER not in roles:
-        raise ValueError(f'Applicant is a {record.get("roles", "")}, not a hacker.')
-    if record.get("decision") == Decision.ACCEPTED:
+        raise ValueError(f'Applicant is a {roles}, not a hacker.')
+    if decision == Decision.ACCEPTED:
         raise ValueError("Applicant decision is accepted and should be checked in.")
-    elif record.get("decision") == Decision.REJECTED:
+    elif decision == Decision.REJECTED:
         raise ValueError("Applicant decision is rejected. Not eligible for hackathon.")
-    if record.get("status", "") == Status.ATTENDING:
+    if status == Status.ATTENDING:
         raise ValueError("Participant has checked in. Not eligible for queue.")
-    elif record.get("status", "") != Status.WAIVER_SIGNED:
-        raise ValueError("Participant has checked in. Not eligible for queue.")
+    elif status != Status.WAIVER_SIGNED:
+        raise ValueError("Participant has not signed waiver. Not eligible for queue.")
 
     update_status = await mongodb_handler.raw_update_one(
         Collection.USERS, {"_id": uid}, {"$set": {"status": Status.QUEUED}}

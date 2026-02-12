c6ed81ec5a30c819128c0b453554e826a87edbb1
Noah Kim <101167833+noahk004@users.noreply.github.com>
2026-02-12 00:16:51 -0800

    Update apps/site/src/app/(main)/schedule/sections/ScheduleScroll.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/site/src/app/(main)/schedule/sections/ScheduleScroll.tsx
+++ apps/site/src/app/(main)/schedule/sections/ScheduleScroll.tsx
@@ -25,7 +25,7 @@ export default function ScheduleScroll({
 		scheduleContainerRef.current?.scrollTo({
 			left: scheduleBarRef.current
 				? scheduleBarRef.current.getBoundingClientRect().width *
-				fixedPos[newPos]
+				  fixedPos[newPos]
 				: 0,
 			behavior: "smooth",
 		});

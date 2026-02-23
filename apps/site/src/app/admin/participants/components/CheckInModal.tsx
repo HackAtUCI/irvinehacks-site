1d63d10a52921b3778fe621206f7d359a049e7b3
Connie Yang <67993935+connieyyy@users.noreply.github.com>
2026-02-23 06:35:42 -0800

    fix: update checkin modal & close walkin waitlist recipients (#837)
    * updated modal instructions and to switch instructions based on type of check in
    
    * updated close walk in email to all applicants with waitlisted decision

--- apps/site/src/app/admin/participants/components/CheckInModal.tsx
+++ apps/site/src/app/admin/participants/components/CheckInModal.tsx
@@ -44,55 +44,68 @@ function CheckInModal({ onDismiss, onConfirm, participant }: ActionModalProps) {
 			header={`Participant Name: ${participant?.first_name} ${participant?.last_name}`}
 		>
 			<SpaceBetween size="s">
-				<div>
-					<p>
-						<strong>General Check-in Instructions</strong>
-					</p>
-					<TextContent>
-						<ul>
+				{selectedType === "accepted" && (
+					<div>
+						<p>
+							<strong>General Check-in Instructions</strong>
+						</p>
+						<TextContent>
 							<ol>
 								<li>
 									Ask for a photo ID and check participant is 18+ years old.
 								</li>
 								<li>Have participant sign the SPFB sheet.</li>
+								<li>
+									Ask participant if they have joined Slack. Add them if
+									necessary.
+								</li>
 								<li>Ask participant to fill in badge.</li>
+								<li>
+									Inform participant regarding the following:
+									<ol>
+										<li>Can head inside ballroom to talk to sponsors.</li>
+										<li>Team formation starts at 7pm at Moss Cove B.</li>
+										<li>Opening ceremony starts at 8pm.</li>
+										<li>
+											Schedule is available on website at
+											<b> irvinehacks.com/schedule.</b>
+										</li>
+									</ol>
+								</li>
 							</ol>
-						</ul>
-					</TextContent>
-				</div>
+						</TextContent>
+					</div>
+				)}
 
-				<div>
-					<p>
-						<strong>Queue Instructions</strong>
-					</p>
-					<TextContent>
-						<ol>
-							<li>
-								Ask for a photo ID and check participant is 18+ years old.
-							</li>
-							<li>
-								Inform participant regarding the following:
-								<ol>
-									<li>Turn on email notifications.</li>
-									<li>
-										Participants have 15 minutes to check in starting from email
-										notification sent or they will have to come in person to
-										rejoin the queue.
-									</li>
-									<li>Waitlist check in starts at 6pm.</li>
-								</ol>
-							</li>
-						</ol>
-						<hr
-							style={{
-								border: "0",
-								borderTop: "1px solid #ddd",
-								margin: "20px 0",
-							}}
-						/>
-						<p>Select check-in type:</p>
-					</TextContent>
-				</div>
+				{selectedType === "waitlisted" && (
+					<div>
+						<p>
+							<strong>Queue Instructions</strong>
+						</p>
+						<TextContent>
+							<ol>
+								<li>
+									Ask for a photo ID and check participant is 18+ years old.
+								</li>
+								<li>
+									Inform participant regarding the following:
+									<ol>
+										<li>Turn on email notifications.</li>
+										<li>
+											Participants have 15 minutes to check in starting from
+											email notification sent or they will have to come in
+											person to rejoin the queue.
+										</li>
+										<li>
+											We will start calling people off waitlist to check in at
+											6pm.
+										</li>
+									</ol>
+								</li>
+							</ol>
+						</TextContent>
+					</div>
+				)}
 
 				<Tiles
 					onChange={({ detail }) => setSelectedType(detail.value)}

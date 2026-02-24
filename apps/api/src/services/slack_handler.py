dbbdd39808bfd69057eae7efba3c7948c26fe47f
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2026-02-23 20:17:47 -0800

    Slack API verification (#840)
    * Created slack handler
    
    * Created slack router and challenge endpoint
    
    * Temp prints
    
    * use log
    
    * Fixed route
    
    * Added unit tests
    
    * Added checks to headers
    
    * Added env variable for docker compose for testing

---
+++ apps/api/src/services/slack_handler.py
@@ -0,0 +1,53 @@
+import os
+import time
+import hashlib
+import hmac
+import json
+from typing import Any
+from logging import getLogger
+
+from fastapi import HTTPException, Request, status
+
+SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
+
+
+log = getLogger(__name__)
+
+
+async def require_slack(request: Request) -> dict[Any, Any]:
+    """Verifies request signature and returns request body"""
+    if not SIGNING_SECRET:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    raw_body_bytes = await request.body()
+
+    timestamp = request.headers.get("X-Slack-Request-Timestamp")
+    slack_signature = request.headers.get("x-slack-signature")
+
+    if not slack_signature or not timestamp or not _is_int(timestamp):
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+    if abs(time.time() - int(timestamp)) > 60 * 5:
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+    raw_body_string = raw_body_bytes.decode("utf-8")
+    sig_basestring = f"v0:{timestamp}:{raw_body_string}".encode("utf-8")
+
+    my_signature = (
+        "v0="
+        + hmac.new(
+            SIGNING_SECRET.encode("utf-8"), sig_basestring, hashlib.sha256
+        ).hexdigest()
+    )
+    if not hmac.compare_digest(my_signature, slack_signature):
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+    return json.loads(raw_body_string) or {}
+
+
+def _is_int(timestamp: str) -> bool:
+    try:
+        int(timestamp)
+    except (ValueError, TypeError):
+        return False
+    return True

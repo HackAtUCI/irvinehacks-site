2af09ea3616e110e8556c867d5287799ad3c21a6
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2026-05-13 18:45:33 -0700

    Excluded voided applications from HackerApplicantsList for organizers but not directors

--- apps/site/src/app/admin/applicants/components/HackerApplicantsList.tsx
+++ apps/site/src/app/admin/applicants/components/HackerApplicantsList.tsx
@@ -23,7 +23,7 @@ import { SelectProps } from "@cloudscape-design/components/select";
 import ApplicantStatus from "@/app/admin/applicants/components/ApplicantStatus";
 
 import UserContext from "@/lib/admin/UserContext";
-import { isDirector, isHackerReviewer } from "@/lib/admin/authorization";
+import { isDirector, hasAdminRole, isHackerReviewer } from "@/lib/admin/authorization";
 import ApplicantReviewerIndicator from "../components/ApplicantReviewerIndicator";
 import HackerThresholdInputs from "../components/HackerThresholdInputs";
 
@@ -56,6 +56,7 @@ function HackerApplicantsList({ hackathonName }: HackerApplicantsListProps) {
 	}
 
 	const isUserDirector = isDirector(roles);
+	const isOrganizer = hasAdminRole(roles)
 
 	const [selectedStatuses, setSelectedStatuses] = useState<Options>([]);
 	const [selectedDecisions, setSelectedDecisions] = useState<Options>([]);
@@ -95,6 +96,12 @@ function HackerApplicantsList({ hackathonName }: HackerApplicantsListProps) {
 		if (
 			selectedStatusValues.includes(Status.Pending) &&
 			applicant.avg_score === OVERQUALIFIED_SCORE
+		)
+			return false;
+
+		if (
+			applicant.status == Status.Voided &&
+			isOrganizer && !isUserDirector
 		)
 			return false;
 

4a8249d97f193428205459508034b2b9ffe9a3c9
Nathan Ong <nathanong1217@gmail.com>
2026-09-22 00:00:25 -0700

    chore

--- apps/api/src/routers/admin.py
+++ apps/api/src/routers/admin.py
@@ -490,7 +490,7 @@ async def hacker_applicants(
             )
         else:
             (
-                applicant_review_processor.include_hacker_app_fields_with_global_and_breakdown(
+                applicant_review_processor.include_hacker_app_fields_with_global_and_breakdown( # noqa: E501
                     record, thresholds["accept"], thresholds["waitlist"]
                 )
             )
